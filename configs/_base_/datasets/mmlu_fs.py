cc5a8adf03e3e7ef5ee09d77f8bdcf938b32ef83
LZHgrla <linzhihao@pjlab.org.cn>
2023-07-12 17:37:36 +0800

    add mmlu dataset configs

---
+++ configs/_base_/datasets/mmlu_fs.py
@@ -0,0 +1,39 @@
+from datasets import load_dataset
+from mmchat.datasets import process_hf_dataset
+from mmengine.dataset import DefaultSampler
+
+
+data_root = 'data/mmlu/'
+
+mmlu_fs_dataset = dict(
+    type=load_dataset,
+    path='json',
+    data_files=dict(
+        val=data_root + 'five_shot_mmlu_val.json',
+        test=data_root + 'five_shot_mmlu_test.json'))
+
+val_mmlu_fs = dict(
+    type=process_hf_dataset,
+    dataset=mmlu_fs_dataset,
+    mode='val')
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    dataset=val_mmlu_fs,
+    sampler=dict(type=DefaultSampler, shuffle=False))
+
+test_mmlu_fs = dict(
+    type=process_hf_dataset,
+    dataset=mmlu_fs_dataset,
+    mode='test')
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=1,
+    dataset=test_mmlu_fs,
+    sampler=dict(type=DefaultSampler, shuffle=False))
+
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+
+val_evaluator = dict(type='MMLUMetric', tokenizer=None, prefix='mmlu_val')
+test_evaluator = dict(type='MMLUMetric', tokenizer=None, prefix='mmlu_test')
