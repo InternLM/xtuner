27a041a01243f25fa10efa7e9b3566d04fdd1cae
HIT-cwh <2892770585@qq.com>
2023-07-17 14:01:13 +0800

    support save lora-related state_dict

--- mmchat/models/algorithms/sft_qlora.py
+++ mmchat/models/algorithms/sft_qlora.py
@@ -1,6 +1,7 @@
 import bitsandbytes as bnb
 import torch
-from peft import get_peft_model, prepare_model_for_kbit_training
+from peft import (PeftType, PromptLearningConfig, get_peft_model,
+                  prepare_model_for_kbit_training)
 
 from mmchat.registry import MODELS
 from .sft import SupervisedFinetune
@@ -47,3 +48,85 @@ class SupervisedQloraFinetune(SupervisedFinetune):
 
     def init_weights(self):
         pass
+
+    def state_dict(self, destination=None, prefix='', keep_vars=False):
+
+        def get_peft_model_state_dict(model,
+                                      state_dict=None,
+                                      adapter_name='default'):
+            # Modified from `https://github.com/huggingface/peft/blob/main/src
+            # /peft/utils/save_and_load.py`
+
+            config = model.peft_config[adapter_name]
+            if state_dict is None:
+                state_dict = model.state_dict()
+            if config.peft_type in (PeftType.LORA, PeftType.ADALORA):
+                # to_return = lora_state_dict(model,
+                #                             bias=model.peft_config.bias)
+                # adapted from `https://github.com/microsoft/LoRA/blob/main/
+                # loralib/utils.py`
+                # to be used directly with the state dict which is necessary
+                # when using DeepSpeed or FSDP
+                bias = config.bias
+                if bias == 'none':
+                    to_return = {
+                        k: state_dict[k]
+                        for k in state_dict if 'lora_' in k
+                    }
+                elif bias == 'all':
+                    to_return = {
+                        k: state_dict[k]
+                        for k in state_dict if 'lora_' in k or 'bias' in k
+                    }
+                elif bias == 'lora_only':
+                    to_return = {}
+                    for k in state_dict:
+                        if 'lora_' in k:
+                            to_return[k] = state_dict[k]
+                            bias_name = k.split('lora_')[0] + 'bias'
+                            if bias_name in state_dict:
+                                to_return[bias_name] = state_dict[bias_name]
+                else:
+                    raise NotImplementedError
+                to_return = {
+                    k: v
+                    for k, v in to_return.items()
+                    if (('lora_' in k and adapter_name in k) or ('bias' in k))
+                }
+                if config.peft_type == PeftType.ADALORA:
+                    rank_pattern = config.rank_pattern
+                    if rank_pattern is not None:
+                        rank_pattern = {
+                            k.replace(f'.{adapter_name}', ''): v
+                            for k, v in rank_pattern.items()
+                        }
+                        config.rank_pattern = rank_pattern
+
+            elif config.peft_type == PeftType.ADAPTION_PROMPT:
+                to_return = {
+                    k: state_dict[k]
+                    for k in state_dict
+                    if k.split('.')[-1].startswith('adaption_')
+                }
+            elif isinstance(config, PromptLearningConfig):
+                to_return = {}
+                if config.inference_mode:
+                    prompt_embeddings = model.prompt_encoder[
+                        adapter_name].embedding.weight
+                else:
+                    prompt_embeddings = model.get_prompt_embedding_to_save(
+                        adapter_name)
+                to_return['prompt_embeddings'] = prompt_embeddings
+            else:
+                raise NotImplementedError
+            if model.modules_to_save is not None:
+                for key, value in state_dict.items():
+                    if any(f'{module_name}.modules_to_save.{adapter_name}' in
+                           key for module_name in model.modules_to_save):
+                        to_return[key.replace('modules_to_save.', '')] = value
+
+            return to_return
+
+        state_dict = super().state_dict()
+        to_return = get_peft_model_state_dict(self.llm, state_dict=state_dict)
+        return to_return
