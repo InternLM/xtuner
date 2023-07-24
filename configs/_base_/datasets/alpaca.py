be6a1ad58819d8476af1bcc89ce4512ba2a3b041
LZH <36994684+LZHgrla@users.noreply.github.com>
2023-07-23 22:31:21 -0700

    [Fix] Fix typo about alpaca (#10)

--- configs/_base_/datasets/alpaca.py
+++ configs/_base_/datasets/alpaca.py
@@ -3,14 +3,14 @@ from mmengine.dataset import DefaultSampler
 
 from mmchat.datasets import process_hf_dataset
 from mmchat.datasets.collate_fns import default_collate_fn
-from mmchat.datasets.map_fns import aplaca_map_fn
+from mmchat.datasets.map_fns import alpaca_map_fn
 
 _alpaca = dict(
     type=process_hf_dataset,
     dataset=dict(type=load_dataset, path='tatsu-lab/alpaca'),
     tokenizer=None,
     max_length=2048,
-    map_fn=aplaca_map_fn,
+    map_fn=alpaca_map_fn,
     remove_columns=['instruction', 'text'],
     concat_to_max_length=False)
 
