057f951184e4bd3db48600a55cebfb0666ff30eb
LZHgrla <linzhihao@pjlab.org.cn>
2023-07-26 17:33:47 +0800

    add arxiv dataset

---
+++ mmchat/datasets/map_fns/arxiv_map_fn.py
@@ -0,0 +1,6 @@
+def arxiv_map_fn(example):
+    PROMPT = ('If you are an expert in writing papers, please generate '
+              "a good paper title for this paper based on other authors' "
+              'descriptions of their abstracts.\n\n'
+              '### Descriptions:\n{abstract}\n\n### Title: ')
+    return {'input': PROMPT.format(**example), 'output': example['title']}
---
+++ tools/data_preprocess/arxiv.py
@@ -0,0 +1,39 @@
+import json
+from datetime import datetime
+
+
+def has_intersection(list1, list2):
+    set1 = set(list1)
+    set2 = set(list2)
+    return len(set1.intersection(set2)) > 0
+
+
+def read_json_file(file_path):
+    data = []
+    with open(file_path) as file:
+        for line in file:
+            try:
+                json_data = json.loads(line)
+                data.append(json_data)
+            except json.JSONDecodeError:
+                print(f'Failed to parse line: {line}')
+    return data
+
+
+file_path = '../data/arxiv-metadata-oai-snapshot.json'
+json_data = read_json_file(file_path)
+
+target_categories = ['cs.AI', 'cs.CL', 'cs.CV']
+from_time = datetime(2020, 1, 1)
+filtered_data = [
+    item for item in json_data
+    if has_intersection(target_categories, item['categories'].split())
+    and datetime.strptime(item['update_date'], '%Y-%m-%d') >= from_time
+]
+
+file_path = '../data/arxiv_postprocess_csAIcsCLcsCV_20200101.json'
+
+with open(file_path, 'w') as file:
+    json.dump(filtered_data, file)
+
+print(f'Save to {file_path}\n{len(filtered_data)} items')
