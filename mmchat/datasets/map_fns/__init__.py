7f565838e83d91d7b9126310bdfe4ffad303724d
LZHgrla <linzhihao@pjlab.org.cn>
2023-08-01 13:48:59 +0800

    fix pre-commit

--- mmchat/datasets/map_fns/__init__.py
+++ mmchat/datasets/map_fns/__init__.py
@@ -1,7 +1,8 @@
-from .dataset_map_fn import alpaca_map_fn, alpaca_zh_map_fn, oasst1_map_fn, arxiv_map_fn
+from .dataset_map_fn import (alpaca_map_fn, alpaca_zh_map_fn, arxiv_map_fn,
+                             oasst1_map_fn)
 from .model_map_fn import internlm_map_fn, llama2_map_fn
 
 __all__ = [
-    'alpaca_map_fn', 'alpaca_zh_map_fn', 'oasst1_map_fn', 'arxiv_map_fn', 'internlm_map_fn',
-    'llama2_map_fn'
+    'alpaca_map_fn', 'alpaca_zh_map_fn', 'oasst1_map_fn', 'arxiv_map_fn',
+    'internlm_map_fn', 'llama2_map_fn'
 ]
