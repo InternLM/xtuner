d95e655b694b3cc5acbe43b875b03c7f923c13c7
LZHgrla <linzhihao@pjlab.org.cn>
2023-08-03 15:31:33 +0800

    update configs

--- configs/llama2/llama2_7b_chat_qlora_alpaca.py
+++ configs/llama2/llama2_7b_chat_qlora_alpaca.py
@@ -6,11 +6,8 @@ with read_base():
     from .._base_.datasets.alpaca_enzh import *  # noqa: F401,F403
     from .._base_.default_runtime import *  # noqa: F401,F403
     from .._base_.models.llama2_7b_chat_qlora import *  # noqa: F401,F403
-    from .._base_.schedules.cosine import *  # noqa: F401,F403
+    from .._base_.schedules.cosine_e1 import *  # noqa: F401,F403
 
 train_dataloader.dataset.tokenizer = tokenizer  # noqa: F405
-
-default_hooks.checkpoint.update(  # noqa: F405
-    dict(by_epoch=False, interval=500, max_keep_ckpts=2))
 
 custom_hooks = [dict(type=LogSampleHook, tokenizer=tokenizer)]  # noqa: F405
