86a580478006906ec81ef7073f4d609ef4dcceac
LZHgrla <linzhihao@pjlab.org.cn>
2023-08-03 22:46:12 +0800

    fix bugs

--- configs/internlm/internlm_chat_7b_qlora_alpaca.py
+++ configs/internlm/internlm_chat_7b_qlora_alpaca.py
@@ -6,7 +6,7 @@ with read_base():
     from .._base_.datasets.alpaca_enzh import *  # noqa: F401,F403
     from .._base_.default_runtime import *  # noqa: F401,F403
     from .._base_.models.internlm_chat_7b_qlora import *  # noqa: F401,F403
-    from .._base_.schedules.cosine_e1 import *  # noqa: F401,F403
+    from .._base_.schedules.cosine_e3 import *  # noqa: F401,F403
 
 train_dataloader.dataset.datasets_kwargs.tokenizer = tokenizer  # noqa: F405
 
