67ff6ba54d7ff2e35ee5e0c3a45a35c4a11a18a9
LZHgrla <linzhihao@pjlab.org.cn>
2023-08-04 10:52:52 +0800

    fix pre-commit

--- tools/model_converters/merge_lora_hf.py
+++ tools/model_converters/merge_lora_hf.py
@@ -2,23 +2,36 @@
 import argparse
 
 import torch
-
-from transformers import AutoModelForCausalLM, AutoTokenizer
 from peft import PeftModel
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Merge a lora to model')
     parser.add_argument('model_name_or_path', help='model name or path')
     parser.add_argument('lora_name_or_path', help='lora name or path')
-    parser.add_argument('save_dir', help='the directory to save the merged model')
+    parser.add_argument(
+        'save_dir', help='the directory to save the merged model')
     args = parser.parse_args()
     return args
 
+
 def main():
     args = parse_args()
-    model = AutoModelForCausalLM.from_pretrained(args.model_name_or_path, torch_dtype=torch.float16, low_cpu_mem_usage=True, device_map='cpu', trust_remote_code=True)
-    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path, trust_remote_code=True)
-    model_unmerged = PeftModel.from_pretrained(model, args.lora_name_or_path, device_map='cpu', torch_dtype=torch.float16, is_trainable=False)
+    model = AutoModelForCausalLM.from_pretrained(
+        args.model_name_or_path,
+        torch_dtype=torch.float16,
+        low_cpu_mem_usage=True,
+        device_map='cpu',
+        trust_remote_code=True)
+    tokenizer = AutoTokenizer.from_pretrained(
+        args.model_name_or_path, trust_remote_code=True)
+    model_unmerged = PeftModel.from_pretrained(
+        model,
+        args.lora_name_or_path,
+        device_map='cpu',
+        torch_dtype=torch.float16,
+        is_trainable=False)
     model_merged = model_unmerged.merge_and_unload()
     model_merged.save_pretrained(args.save_dir)
     tokenizer.save_pretrained(args.save_dir)
