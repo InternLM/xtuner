42c1ad28a768f23c126b2d50939342ecc7872471
HIT-cwh <2892770585@qq.com>
2023-08-07 17:16:31 +0800

    add copyright

--- mmchat/datasets/map_fns/dataset_map_fn/openorca_map_fn.py
+++ mmchat/datasets/map_fns/dataset_map_fn/openorca_map_fn.py
@@ -1,3 +1,4 @@
+# Copyright (c) OpenMMLab. All rights reserved.
 def openorca_map_fn(example):
     PROMPT = ('Below is an instruction that describes a task. '
               'Write a response that appropriately completes the request.\n\n'
