7af533fed2ae9091a55b72c46531de5bb516644c
humu789 <humu@pjlab.org.cn>
2023-08-07 17:32:18 +0800

    unify examples & updated deepspeed pipeline

--- examples/run_train_alpaca-qlora_with_mmchat_deepspeed.sh
+++ examples/run_train_alpaca-qlora_with_mmchat_deepspeed.sh
@@ -1,1 +1,4 @@
-CUDA_VISIBLE_DEVICES=1,2,3,4 deepspeed train_alpaca-qlora_with_mmchat_deepspeed.py
+CUDA_VISIBLE_DEVICES=1,2,3,4 deepspeed train_alpaca-qlora_with_mmchat_deepspeed.py \
+    --model_name_or_path internlm/internlm-7b \
+    --use_qlora False \
+    --use_lora True \
--- examples/train_alpaca-qlora_with_mmchat_deepspeed.py
+++ examples/train_alpaca-qlora_with_mmchat_deepspeed.py
@@ -8,9 +8,10 @@ from mmengine.config import Config
 from mmengine.runner import Runner
 from peft import LoraConfig
 from tqdm import tqdm
-from transformers import BitsAndBytesConfig
+from transformers import BitsAndBytesConfig, AutoTokenizer
 
 from mmchat.models import SupervisedFinetune
+from data_utils import get_train_dataloader
 
 
 def get_argument_parser():
@@ -18,9 +19,13 @@ def get_argument_parser():
     parser.add_argument(
         '--model_name_or_path', type=str, default='internlm/internlm-7b')
     parser.add_argument(
+        '--use_qlora', type=bool, default=True)
+    parser.add_argument(
+        '--use_lora', type=bool, default=False)
+    parser.add_argument(
         '--dataset_cfg_path',
         type=str,
-        default='../configs/internlm/internlm_7b/internlm_7b_qlora_alpaca.py',
+        default='../configs/_base_/datasets/alpaca.py',
         help='Path to mmchat dataset config')
     parser.add_argument(
         '--deepspeed_config',
@@ -72,26 +77,42 @@ def print_rank_0(msg, rank=0):
 
 def train():
     # build model
-    quantization_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        load_in_8bit=False,
-        llm_int8_threshold=6.0,
-        llm_int8_has_fp16_weight=False,
-        bnb_4bit_compute_dtype=torch.float16,
-        bnb_4bit_use_double_quant=True,
-        bnb_4bit_quant_type='nf4')
+    if args.use_qlora:
+        quantization_config = BitsAndBytesConfig(
+            load_in_4bit=True,
+            load_in_8bit=False,
+            llm_int8_threshold=6.0,
+            llm_int8_has_fp16_weight=False,
+            bnb_4bit_compute_dtype=torch.float16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type='nf4')
+        lora_config = LoraConfig(
+            r=64,
+            lora_alpha=16,
+            lora_dropout=0.1,
+            bias='none',
+            task_type='CAUSAL_LM')
+    elif not args.use_qlora and args.use_lora:
+        quantization_config = None
+        lora_config = LoraConfig(
+            r=64,
+            lora_alpha=16,
+            lora_dropout=0.1,
+            bias='none',
+            task_type='CAUSAL_LM')
+    else:
+        quantization_config = None
+        lora_config = None
+        
     llm = transformers.AutoModelForCausalLM.from_pretrained(
         args.model_name_or_path,
         quantization_config=quantization_config,
         trust_remote_code=True
     )
-    lora_config = LoraConfig(
-        r=64,
-        lora_alpha=16,
-        lora_dropout=0.1,
-        bias='none',
-        task_type='CAUSAL_LM')
-    model = SupervisedFinetune(llm=llm, lora=lora_config)
+    tokenizer = AutoTokenizer.from_pretrained(args.model_name_or_path,
+                                              trust_remote_code=True)
+
+    model = SupervisedFinetune(llm=llm, lora=lora_config, tokenizer=tokenizer)
 
     # build deepspeed engine
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
@@ -102,8 +123,7 @@ def train():
         model_parameters=model_parameters)
 
     # build dataloader
-    dataset_cfg = Config.fromfile(args.dataset_cfg_path)
-    train_dataloader = Runner.build_dataloader(dataset_cfg.train_dataloader)
+    train_dataloader = get_train_dataloader(args.dataset_cfg_path, tokenizer)
 
     # training
     model_engine.train()
