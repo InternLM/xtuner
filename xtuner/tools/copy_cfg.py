84b70665615e25a88bcd0cf6271f91ba685f69d4
LZHgrla <36994684+LZHgrla@users.noreply.github.com>
2023-08-17 17:57:02 +0800

    [Improve] Improve cli (#34)
    * add match for list-cfg
    
    * use python
    
    * add copy suffix for copy-cfg
    
    * fix pre-commit
    
    * support any ext in copy-cfg

--- xtuner/tools/copy_cfg.py
+++ xtuner/tools/copy_cfg.py
@@ -16,11 +16,17 @@ def parse_args():
     return args
 
 
+def add_copy_suffix(string):
+    file_name, ext = os.path.splitext(string)
+    return f'{file_name}_copy{ext}'
+
+
 def main():
     args = parse_args()
     mkdir_or_exist(args.save_dir)
     config_path = cfgs_name_path[args.config_name]
-    save_path = os.path.join(args.save_dir, os.path.basename(config_path))
+    save_path = os.path.join(args.save_dir,
+                             add_copy_suffix(os.path.basename(config_path)))
     shutil.copyfile(config_path, save_path)
     print(f'Copy to {save_path}')
 
