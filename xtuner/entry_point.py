e0bd7fe982c79082ca742056356672eadf725515
LZHgrla <linzhihao@pjlab.org.cn>
2023-08-17 11:05:51 +0800

    use python

--- xtuner/entry_point.py
+++ xtuner/entry_point.py
@@ -183,7 +183,7 @@ def cli():
             if callable(module):
                 module()
             else:
-                subprocess.run(['python3', module] + args[n_arg + 1:])
+                subprocess.run(['python', module] + args[n_arg + 1:])
         except Exception as e:
             print_log(f"WARNING: command error: '{e}'!", 'current',
                       logging.WARNING)
@@ -207,7 +207,7 @@ def cli():
                 ]
                 # remove GPUS arg
                 del args[n_arg + 2]
-                subprocess.run(['python3', '-m', 'torch.distributed.launch'] +
+                subprocess.run(['python', '-m', 'torch.distributed.launch'] +
                                dist_args + [module] + args[n_arg + 1:] +
                                ['--launcher', 'pytorch'])
         except Exception as e:
