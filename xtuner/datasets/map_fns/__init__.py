aca8b6db6fe350fe52dbf4d05014f45263965e3b
HIT-cwh <2892770585@qq.com>
2023-08-25 22:10:27 +0800

    decouple map_fn

--- xtuner/datasets/map_fns/__init__.py
+++ xtuner/datasets/map_fns/__init__.py
@@ -1,3 +1,3 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .dataset_map_fn import *  # noqa: F401, F403
-from .prompt_template_map_fn import *  # noqa: F401, F403
+from .prompt_template_map_fn import prompt_template_map_fn  # noqa: F401
