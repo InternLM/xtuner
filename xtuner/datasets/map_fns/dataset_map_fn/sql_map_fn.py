48200f16f69c0fbd02b2b6d304ce7cdcacd19768
LZHgrla <36994684+LZHgrla@users.noreply.github.com>
2023-08-28 10:27:39 +0800

    Update sql_map_fn.py

--- xtuner/datasets/map_fns/dataset_map_fn/sql_map_fn.py
+++ xtuner/datasets/map_fns/dataset_map_fn/sql_map_fn.py
@@ -1,10 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 def sql_map_fn(example):
-    context = example['context']
-    question = example['question']
     return {
         'conversation': [{
-            'input': f'{context}\n{question}',
-            'output': example['answer']
+            'input': '{context}\n{question}'.format(**example),
+            'output': '{answer}'.format(**example)
         }]
     }
