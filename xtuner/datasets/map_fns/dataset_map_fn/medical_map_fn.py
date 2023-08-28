5e780206cdab38b319f43f3defe72372365837df
HIT-cwh <2892770585@qq.com>
2023-08-28 19:11:18 +0800

    fix medical map_fn

--- xtuner/datasets/map_fns/dataset_map_fn/medical_map_fn.py
+++ xtuner/datasets/map_fns/dataset_map_fn/medical_map_fn.py
@@ -1,19 +1,12 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 def medical_map_fn(example):
-    PROMPT = {
-        'with_input': ('如果你是一名医生，请根据患者的描述回答医学问题。\n\n'
-                       '### Input: {instruction}. {input}\n\n### Response: '),
-        'without_input': ('如果你是一名医生，请根据患者的描述回答医学问题。\n\n'
-                          '### Input: {instruction}\n\n### Response: '),
-    }
-    if example.get('input', '') != '':
-        prompt_template = PROMPT['with_input']
-    else:
-        prompt_template = PROMPT['without_input']
-
+    input = example.get('input', '')
+    instruction = example['instruction']
     return {
         'conversation': [{
-            'input': prompt_template.format(**example),
-            'output': example['output']
+            'input':
+            f'{instruction}. {input}' if input != '' else instruction,
+            'output':
+            example['output']
         }]
     }
