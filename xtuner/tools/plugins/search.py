594d17c915483691d36fc1211adc599aaa4b30ab
LZHgrla <linzhihao@pjlab.org.cn>
2023-08-28 17:15:44 +0800

    use pure search api

--- xtuner/tools/plugins/search.py
+++ xtuner/tools/plugins/search.py
@@ -2,8 +2,7 @@
 import os
 import sys
 
-from lagent.actions import GoogleSearch
-from lagent.schema import ActionStatusCode
+import requests
 
 try:
     SERPER_API_KEY = os.environ['SERPER_API_KEY']
@@ -12,17 +11,46 @@ except Exception:
           'set it using `export SERPER_API_KEY=xxx`.')
     sys.exit(1)
 
-search_engine = GoogleSearch(SERPER_API_KEY)
+
+def parse_results(results, k=10):
+    snippets = []
+
+    for result in results['organic'][:k]:
+        if 'snippet' in result:
+            snippets.append(result['snippet'])
+        for attribute, value in result.get('attributes', {}).items():
+            snippets.append(f'{attribute}: {value}.')
+    return snippets
 
 
-def Search(q):
-    action_return = search_engine(q)
-    if action_return.state == ActionStatusCode.SUCCESS:
-        response = eval(action_return.result['text'])
+def search(api_key, search_term, **kwargs):
+    headers = {
+        'X-API-KEY': api_key,
+        'Content-Type': 'application/json',
+    }
+    params = {
+        'q': search_term,
+        **{key: value
+           for key, value in kwargs.items() if value is not None},
+    }
+    try:
+        response = requests.post(
+            'https://google.serper.dev/search',
+            headers=headers,
+            params=params,
+            timeout=5)
+    except Exception as e:
+        return -1, str(e)
+    return response.status_code, response.json()
+
+
+def Search(q, k=10):
+    status_code, response = search(SERPER_API_KEY, q)
+    if status_code != 200:
+        ret = 'None\n'
     else:
-        response = action_return.errmsg
-
-    ret = ''
-    for idx, res in enumerate(response):
-        ret += f"<|{idx+1}|>: '{res}'\n"
+        text = parse_results(response, k=k)
+        ret = ''
+        for idx, res in enumerate(text):
+            ret += f"<|{idx+1}|>: '{res}'\n"
     return ret
