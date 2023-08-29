d347c1c36423d082c3e2930f00b23290ce0d1b92
LZHgrla <linzhihao@pjlab.org.cn>
2023-08-29 15:07:20 +0800

    add baichuan_7b configs

--- xtuner/configs/baichuan/baichuan_7b/baichuan_7b_qlora_arxiv_gentitle_e3.py
+++ xtuner/configs/baichuan/baichuan_7b/baichuan_7b_qlora_arxiv_gentitle_e3.py
@@ -1,5 +1,6 @@
 import torch
 from bitsandbytes.optim import PagedAdamW32bit
+from datasets import load_dataset
 from mmengine.dataset import DefaultSampler
 from mmengine.hooks import (CheckpointHook, DistSamplerSeedHook, IterTimerHook,
                             LoggerHook, ParamSchedulerHook)
@@ -8,8 +9,9 @@ from peft import LoraConfig
 from transformers import (AutoModelForCausalLM, AutoTokenizer,
                           BitsAndBytesConfig)
 
-from xtuner.dataset import MOSSSFTDataset
+from xtuner.dataset import process_hf_dataset
 from xtuner.dataset.collate_fns import default_collate_fn
+from xtuner.dataset.map_fns import arxiv_map_fn, template_map_fn_factory
 from xtuner.engine import DatasetInfoHook, EvaluateChatHook
 from xtuner.model import SupervisedFinetune
 from xtuner.utils import PROMPT_TEMPLATE
@@ -19,18 +21,20 @@ from xtuner.utils import PROMPT_TEMPLATE
 #######################################################################
 # Model
 pretrained_model_name_or_path = 'baichuan-inc/Baichuan-7B'
-bot_name = 'Llama2'
 
 # Data
-# Download data from https://huggingface.co/datasets/fnlp/moss-003-sft-data
-moss_sft_plugins_path = './data/conversations_with_tools_with_inner_instruction_no_text2image_train_all_random_meta0.5_0.1_0.01_moss_0709.jsonl'  # noqa: E501
+# 1. Download data from https://kaggle.com/datasets/Cornell-University/arxiv
+# 2. Process data with `./tools/data_preprocess/arxiv.py`
+data_path = './data/arxiv_postprocess_csAIcsCLcsCV_20200101.json'
+prompt_template = PROMPT_TEMPLATE.title
 max_length = 2048
+pack_to_max_length = True
 
 # Scheduler & Optimizer
 batch_size = 1  # per_device
 accumulative_counts = 16
 dataloader_num_workers = 0
-max_epochs = 1
+max_epochs = 3
 optim_type = PagedAdamW32bit
 lr = 2e-4
 betas = (0.9, 0.999)
@@ -38,10 +42,41 @@ weight_decay = 0
 max_norm = 1  # grad clip
 
 # Evaluate the generation performance during the training
-prompt_template = PROMPT_TEMPLATE.moss_sft
 evaluation_freq = 500
 evaluation_inputs = [
-    '一个球体的表面积是384平方厘米，求它的体积。', '今有鸡兔同笼，上有二十头，下有六十二足， 问鸡兔各几何？', '介绍一下比尔盖茨'
+    ('We present InternLM, a multilingual foundational language '
+     'model with 104B parameters. InternLM is pre-trained on a large '
+     'corpora with 1.6T tokens with a multi-phase progressive '
+     'process, and then fine-tuned to align with human preferences. '
+     'We also developed a training system called Uniscale-LLM for '
+     'efficient large language model training. The evaluation on a '
+     'number of benchmarks shows that InternLM achieves '
+     'state-of-the-art performance in multiple aspects, including '
+     'knowledge understanding, reading comprehension, mathematics, '
+     'and coding. With such well-rounded capabilities, InternLM '
+     'achieves outstanding performances on comprehensive exams, '
+     'including MMLU, AGIEval, C-Eval and GAOKAO-Bench, without '
+     'resorting to external tools. On these benchmarks, InternLM '
+     'not only significantly outperforms open-source models, but '
+     'also obtains superior performance compared to ChatGPT. Also, '
+     'InternLM demonstrates excellent capability of understanding '
+     'Chinese language and Chinese culture, which makes it a '
+     'suitable foundation model to support Chinese-oriented language '
+     'applications. This manuscript gives a detailed study of '
+     'our results, with benchmarks and examples across a diverse '
+     'set of knowledge domains and tasks.'),
+    ('In this work, we develop and release Llama 2, a collection of '
+     'pretrained and fine-tuned large language models (LLMs) ranging '
+     'in scale from 7 billion to 70 billion parameters.\nOur '
+     'fine-tuned LLMs, called LLAMA 2-CHAT, are optimized for '
+     'dialogue use cases. Our models outperform open-source chat '
+     'models on most benchmarks we tested, and based on our human '
+     'evaluations for helpfulness and safety, may be a suitable '
+     'substitute for closedsource models. We provide a detailed '
+     'description of our approach to fine-tuning and safety '
+     'improvements of LLAMA 2-CHAT in order to enable the community '
+     'to build on our work and contribute to the responsible '
+     'development of LLMs.')
 ]
 
 #######################################################################
@@ -81,11 +116,17 @@ model = dict(
 #                      PART 3  Dataset & Dataloader                   #
 #######################################################################
 train_dataset = dict(
-    type=MOSSSFTDataset,
-    data_file=moss_sft_plugins_path,
-    bot_name=bot_name,
+    type=process_hf_dataset,
+    dataset=dict(
+        type=load_dataset, path='json', data_files=dict(train=data_path)),
     tokenizer=tokenizer,
-    max_length=max_length)
+    max_length=max_length,
+    dataset_map_fn=arxiv_map_fn,
+    template_map_fn=dict(
+        type=template_map_fn_factory, template=prompt_template),
+    remove_unused_columns=True,
+    shuffle_before_pack=True,
+    pack_to_max_length=pack_to_max_length)
 
 train_dataloader = dict(
     batch_size=batch_size,
@@ -129,7 +170,6 @@ custom_hooks = [
         type=EvaluateChatHook,
         tokenizer=tokenizer,
         every_n_iters=evaluation_freq,
-        stop_word='<eoc>',
         evaluation_inputs=evaluation_inputs,
         instruction=prompt_template.INSTRUCTION_START)
 ]
