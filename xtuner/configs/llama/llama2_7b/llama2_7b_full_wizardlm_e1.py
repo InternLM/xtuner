498ac4f6bfb018bb513dace1922eb92339d1df5c
LZHgrla <linzhihao@pjlab.org.cn>
2023-08-30 15:16:13 +0800

    fix pre-commit

--- xtuner/configs/llama/llama2_7b/llama2_7b_full_wizardlm_e1.py
+++ xtuner/configs/llama/llama2_7b/llama2_7b_full_wizardlm_e1.py
@@ -10,7 +10,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 from xtuner.dataset import process_hf_dataset
 from xtuner.dataset.collate_fns import default_collate_fn
-from xtuner.dataset.map_fns import wizardlm_map_fn, template_map_fn_factory
+from xtuner.dataset.map_fns import template_map_fn_factory, wizardlm_map_fn
 from xtuner.engine import DatasetInfoHook, EvaluateChatHook
 from xtuner.model import SupervisedFinetune
 from xtuner.utils import PROMPT_TEMPLATE
