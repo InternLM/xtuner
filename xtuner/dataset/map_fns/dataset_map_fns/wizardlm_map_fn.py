493cea21258209bd8a02755827d592609f68adf4
HIT-cwh <2892770585@qq.com>
2023-08-30 10:38:20 +0800

    fix potential bugs

--- xtuner/dataset/map_fns/dataset_map_fns/wizardlm_map_fn.py
+++ xtuner/dataset/map_fns/dataset_map_fns/wizardlm_map_fn.py
@@ -5,6 +5,13 @@ def wizardlm_map_fn(example):
         # Skip the first one if it is not from human
         data = data[1:]
 
+    if len(data) % 2:
+        # The last round of conversation solely consists of input
+        # without any output.
+        # Discard the input part of the last round, as this part is ignored in
+        # the loss calculation.
+        data.pop()
+
     conversation = []
     for i in range(0, len(data), 2):
         assert data[i]['from'] == 'human' and data[i + 1]['from'] == 'gpt'
