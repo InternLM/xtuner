742b0e0c0385b6918e7ef6a034251e04171ee718
LZHgrla <linzhihao@pjlab.org.cn>
2023-09-04 19:38:10 +0800

    modify entry_point

--- xtuner/entry_point.py
+++ xtuner/entry_point.py
@@ -8,10 +8,10 @@ import sys
 from mmengine.logging import print_log
 
 import xtuner
-from xtuner.tools import chat, chat_hf, copy_cfg, list_cfg, test, train
+from xtuner.tools import chat, copy_cfg, list_cfg, test, train
 from xtuner.tools.data_preprocess import arxiv as arxiv_preprocess
 from xtuner.tools.model_converters import (adapter_pth2hf, merge_adapter,
-                                           merge_adapter_hf, split_hf_llm)
+                                           split_hf_llm)
 
 # Define valid modes
 MODES = ('list-cfg', 'copy-cfg', 'train', 'test', 'chat', 'convert',
@@ -37,17 +37,13 @@ CLI_HELP_MSG = \
             xtuner train $CONFIG
         3-2. Fine-tune LLMs by multiple GPUs:
             NPROC_PER_NODE=$NGPUS NNODES=$NNODES NODE_RANK=$NODE_RANK PORT=$PORT ADDR=$ADDR xtuner dist_train $CONFIG $GPUS
-        4-1. Chat with LLMs with HuggingFace's model and adapter:
-            xtuner chat hf $NAME_OR_PATH_TO_HF_MODEL --adapter $NAME_OR_PATH_TO_HF_ADAPTER --prompt-template $PROMPT_TEMPLATE
-        4-2. Chat with LLMs with XTuner's config and adapter:
-            xtuner chat xtuner $CONFIG --adapter $PATH_TO_PTH_ADAPTER --prompt $PROMPT_TEMPLATE
+        4. Chat with LLMs with HuggingFace's model and adapter:
+            xtuner chat $NAME_OR_PATH_TO_HF_MODEL --adapter $NAME_OR_PATH_TO_HF_ADAPTER --prompt-template $PROMPT_TEMPLATE
         5-1. Convert the pth adapter to HuggingFace's adapter:
             xtuner convert adapter_pth2hf $CONFIG $PATH_TO_PTH_ADAPTER $SAVE_PATH_TO_HF_ADAPTER
         5-2. Merge the HuggingFace's adapter to the pretrained LLM:
-            xtuner convert merge_adapter_hf $NAME_OR_PATH_TO_HF_MODEL $NAME_OR_PATH_TO_HF_ADAPTER $SAVE_PATH
-        5-3. Merge the XTuner's adapter to the pretraiend LLM:
-            xtuner convert merge_adapter $CONFIG $NAME_OR_PATH_TO_HF_ADAPTER $SAVE_PATH
-        5-4. Split HuggingFace's LLM to the smallest sharded one:
+            xtuner convert merge_adapter $NAME_OR_PATH_TO_HF_MODEL $NAME_OR_PATH_TO_HF_ADAPTER $SAVE_PATH
+        5-3. Split HuggingFace's LLM to the smallest sharded one:
             xtuner convert split_hf_llm $NAME_OR_PATH_TO_HF_MODEL $SAVE_PATH
         6-1. Preprocess arxiv dataset:
             xtuner preprocess arxiv $SRC_FILE $DST_FILE --start-date $START_DATE --categories $CATEGORIES
@@ -76,11 +72,8 @@ CONVERT_HELP_MSG = \
         1. Convert the pth adapter to HuggingFace's adapter:
             xtuner convert adapter_pth2hf $CONFIG $PATH_TO_PTH_ADAPTER $SAVE_PATH_TO_HF_ADAPTER
         2. Merge the HuggingFace's adapter to the pretrained LLM:
-            xtuner convert merge_adapter_hf $NAME_OR_PATH_TO_HF_MODEL $NAME_OR_PATH_TO_HF_ADAPTER $SAVE_PATH
-        3. Merge the XTuner's
-        adapter to the pretraiend LLM:
-            xtuner convert merge_adapter $CONFIG $NAME_OR_PATH_TO_HF_ADAPTER $SAVE_PATH
-        4. Split HuggingFace's LLM to the smallest sharded one:
+            xtuner convert merge_adapter $NAME_OR_PATH_TO_HF_MODEL $NAME_OR_PATH_TO_HF_ADAPTER $SAVE_PATH
+        3. Split HuggingFace's LLM to the smallest sharded one:
             xtuner convert split_hf_llm $NAME_OR_PATH_TO_HF_MODEL $SAVE_PATH
 
     GitHub: https://github.com/InternLM/xtuner
@@ -105,27 +98,6 @@ PREPROCESS_HELP_MSG = \
     GitHub: https://github.com/InternLM/xtuner
     """  # noqa: E501
 
-
-CHAT_HELP_MSG = \
-    f"""
-    Arguments received: {str(['xtuner'] + sys.argv[1:])}. xtuner commands use the following syntax:
-
-        xtuner MODE MODE_ARGS ARGS
-
-        Where   MODE (required) is one of {MODES}
-                MODE_ARG (optional) is the argument for specific mode
-                ARGS (optional) are the arguments for specific command
-
-    Some usages for chat: (See more by using -h for specific command!)
-
-        1. Chat with LLMs with HuggingFace's model and adapter:
-            xtuner chat hf $NAME_OR_PATH_TO_HF_MODEL --adapter $NAME_OR_PATH_TO_HF_ADAPTER --prompt-template $PROMPT_TEMPLATE
-        2. Chat with LLMs with XTuner's config and adapter:
-            xtuner chat xtuner internlm_7b_qlora_alpaca --adapter $PATH_TO_PTH_ADAPTER --prompt $PROMPT_TEMPLATE
-
-    GitHub: https://github.com/InternLM/xtuner
-    """  # noqa: E501
-
 special = {
     'help': lambda: print_log(CLI_HELP_MSG, 'current'),
     'version': lambda: print_log(xtuner.__version__, 'current')
@@ -143,16 +115,10 @@ modes = {
     'copy-cfg': copy_cfg.__file__,
     'train': train.__file__,
     'test': test.__file__,
-    'chat': {
-        'hf': chat_hf.__file__,
-        'xtuner': chat.__file__,
-        '--help': lambda: print_log(CHAT_HELP_MSG, 'current'),
-        '-h': lambda: print_log(CHAT_HELP_MSG, 'current')
-    },
+    'chat': chat.__file__,
     'convert': {
         'adapter_pth2hf': adapter_pth2hf.__file__,
         'merge_adapter': merge_adapter.__file__,
-        'merge_adapter_hf': merge_adapter_hf.__file__,
         'split_hf_llm': split_hf_llm.__file__,
         '--help': lambda: print_log(CONVERT_HELP_MSG, 'current'),
         '-h': lambda: print_log(CONVERT_HELP_MSG, 'current')
