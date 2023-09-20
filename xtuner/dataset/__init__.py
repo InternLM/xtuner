33d6eb51c19fa9514f4402941250b7ceee8818ba
LZHgrla <linzhihao@pjlab.org.cn>
2023-09-20 18:53:35 +0800

    add process_ms_dataset

--- xtuner/dataset/__init__.py
+++ xtuner/dataset/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .concat_dataset import ConcatDataset
 from .huggingface import process_hf_dataset
+from .modelscope import process_ms_dataset
 from .moss_sft import MOSSSFTDataset
 
-__all__ = ['process_hf_dataset', 'ConcatDataset', 'MOSSSFTDataset']
+__all__ = [
+    'process_hf_dataset', 'ConcatDataset', 'MOSSSFTDataset',
+    'process_ms_dataset'
+]
