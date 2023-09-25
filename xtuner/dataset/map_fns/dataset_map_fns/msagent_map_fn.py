11b52606927fdf015c55eb2489a915781bf3f46b
LZHgrla <linzhihao@pjlab.org.cn>
2023-09-25 21:36:11 +0800

    fix

--- xtuner/dataset/map_fns/dataset_map_fns/msagent_map_fn.py
+++ xtuner/dataset/map_fns/dataset_map_fns/msagent_map_fn.py
@@ -5,11 +5,11 @@ def msagent_map_fn(example):
     input_text = ''
     for t in text:
         if t['from'] == 'system':
-            input_text += f"<|system|>:{t['value']}\n\n"
+            input_text += f"<|System|>:{t['value']}\n\n"
         elif t['from'] == 'user':
-            input_text += f"<|user|>:{t['value']}\n\n"
+            input_text += f"<|User|>:{t['value']}\n\n"
         elif t['from'] == 'assistant':
-            input_text += '<|assistant|>:'
+            input_text += '<|Bot|>:'
             conversation.append({'input': input_text, 'output': t['value']})
             input_text = ''
     return {'conversation': conversation}
