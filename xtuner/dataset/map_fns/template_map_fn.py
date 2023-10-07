726e400301d93ed028b5a235b7b40f6ace86e76d
LZHgrla <linzhihao@pjlab.org.cn>
2023-10-07 14:49:06 +0800

    support system for template

--- xtuner/dataset/map_fns/template_map_fn.py
+++ xtuner/dataset/map_fns/template_map_fn.py
@@ -6,13 +6,12 @@ def template_map_fn(example, template):
     conversation = example.get('conversation', [])
     for i, single_turn_conversation in enumerate(conversation):
         input = single_turn_conversation['input']
-        if i == 0:
-            single_turn_conversation[
-                'input'] = template.INSTRUCTION_START.format(
-                    input=input, round=i + 1)
-        else:
-            single_turn_conversation['input'] = template.INSTRUCTION.format(
-                input=input, round=i + 1)
+        input = template.INSTRUCTION.format(input=input, round=i + 1)
+        system = single_turn_conversation.get('system', '')
+        if system != '':
+            system = template.SYSTEM.format(system=system)
+            input = system + input
+        single_turn_conversation['input'] = input
     return {'conversation': conversation}
 
 
