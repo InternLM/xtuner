a574ded9734ac19627eb93d84e8c607c2369d3a4
LZHgrla <linzhihao@pjlab.org.cn>
2023-10-07 14:49:20 +0800

    fix alpaca map_fn

--- xtuner/dataset/map_fns/dataset_map_fns/alpaca_map_fn.py
+++ xtuner/dataset/map_fns/dataset_map_fns/alpaca_map_fn.py
@@ -1,13 +1,16 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+ALPACA_SYSTEM = ('Below is an instruction that describes a task. '
+                 'Write a response that appropriately completes the request.')
+
+
 def alpaca_map_fn(example):
     if example.get('output') == '<nooutput>':
-        return {'conversation': [{'input': '', 'output': ''}]}
+        return {'conversation': []}
     else:
         return {
             'conversation': [{
-                'input':
-                '{instruction}\n{input}'.format(**example),
-                'output':
-                example['output']
+                'system': ALPACA_SYSTEM,
+                'input': f"{example['instruction']}\n{example['input']}",
+                'output': example['output']
             }]
         }
