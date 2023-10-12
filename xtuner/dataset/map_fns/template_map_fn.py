6be5f0a089f0100abc7cb6b91d30aef611d8c10a
LZHgrla <linzhihao@pjlab.org.cn>
2023-10-12 18:34:12 +0800

    support system-output text

--- xtuner/dataset/map_fns/template_map_fn.py
+++ xtuner/dataset/map_fns/template_map_fn.py
@@ -5,13 +5,19 @@ from functools import partial
 def template_map_fn(example, template):
     conversation = example.get('conversation', [])
     for i, single_turn_conversation in enumerate(conversation):
-        input = single_turn_conversation['input']
-        input = template.INSTRUCTION.format(input=input, round=i + 1)
+        input_text = ''
+        input = single_turn_conversation.get('input', '')
+        if input != '' and input is not None:
+            input = template.INSTRUCTION.format(input=input, round=i + 1)
+            input_text += input
+            instruction_postfix = ''
+        else:
+            instruction_postfix = template.INSTRUCTION.split('{input}')[-1]
         system = single_turn_conversation.get('system', '')
-        if system != '':
+        if system != '' and system is not None:
             system = template.SYSTEM.format(system=system)
-            input = system + input
-        single_turn_conversation['input'] = input
+            input_text = system + input_text
+        single_turn_conversation['input'] = input_text + instruction_postfix
     return {'conversation': conversation}
 
 
