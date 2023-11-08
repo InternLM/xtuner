b92f07549a760599fbefe4857042a894e4a59769
LZHgrla <linzhihao@pjlab.org.cn>
2023-11-08 15:06:29 +0800

    update

--- xtuner/model/llava.py
+++ xtuner/model/llava.py
@@ -8,8 +8,7 @@ from mmengine.runner import load_checkpoint
 from peft import get_peft_model, prepare_model_for_kbit_training
 
 from ..registry import BUILDER
-from .modules import dispatch_modules
-from .projector import ProjectorConfig, ProjectorModel
+from .modules import ProjectorConfig, ProjectorModel, dispatch_modules
 from .utils import (LoadWoInit, find_all_linear_names,
                     get_peft_model_state_dict, guess_load_checkpoint,
                     make_inputs_require_grad,
