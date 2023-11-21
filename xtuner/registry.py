bd5a4dcfede1ee5febe6017b482f4f5ee53c11a3
whcao <41630003+HIT-cwh@users.noreply.github.com>
2023-11-21 20:41:00 +0800

    [Bug] Resolve the bug introduced by higher versions of DeepSpeed. (#240)
    * Resolve the bug introduced by higher versions of DeepSpeed.
    
    * assert hasattr(wrapper.model, 'data_preprocessor')

--- xtuner/registry.py
+++ xtuner/registry.py
@@ -1,9 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from mmengine.registry import STRATEGIES as MMENGINE_STRATEGIES
 from mmengine.registry import Registry
 
-__all__ = ['BUILDER', 'MAP_FUNC', 'STRATEGIES']
+__all__ = ['BUILDER', 'MAP_FUNC']
 
 BUILDER = Registry('builder')
 MAP_FUNC = Registry('map_fn')
-STRATEGIES = Registry('strategy', parent=MMENGINE_STRATEGIES)
