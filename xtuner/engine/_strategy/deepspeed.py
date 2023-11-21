f22eb6662fcd2ad059a34b664daf06303b35cd68
whcao <41630003+HIT-cwh@users.noreply.github.com>
2023-11-21 20:41:00 +0800

    [Bug] Resolve the bug introduced by higher versions of DeepSpeed. (#240)
    * Resolve the bug introduced by higher versions of DeepSpeed.
    
    * assert hasattr(wrapper.model, 'data_preprocessor')

--- xtuner/engine/_strategy/deepspeed.py
+++ xtuner/engine/_strategy/deepspeed.py
@@ -1,9 +1,6 @@
 from mmengine._strategy import DeepSpeedStrategy as MMEngineDeepSpeedStrategy
 
-from xtuner.registry import STRATEGIES
 
-
-@STRATEGIES.register_module()
 class DeepSpeedStrategy(MMEngineDeepSpeedStrategy):
 
     def __init__(self, *args, **kwargs):
@@ -13,3 +10,12 @@ class DeepSpeedStrategy(MMEngineDeepSpeedStrategy):
 
         # hf_deepspeed_config has to be saved as an attribute.
         self.hf_deepspeed_config = HfDeepSpeedConfig(self.config)
+
+    def _wrap_model(self, model):
+        wrapper = super()._wrap_model(model)
+        # hard code for deepspeed zero3
+        # When utilizing Zero3, the model isn't allocated to CUDA within the
+        # `deepspeed.initialize` process.
+        assert hasattr(wrapper.model, 'data_preprocessor')
+        wrapper.model.data_preprocessor.cuda()
+        return wrapper
