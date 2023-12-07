ac80d1a513f0bc29c601ca5305c1a692380516ab
linzhihao <linzhihao@pjlab.org.cn>
2023-12-07 11:18:53 +0800

    revert invalid input

--- xtuner/dataset/map_fns/template_map_fn.py
+++ xtuner/dataset/map_fns/template_map_fn.py
@@ -7,19 +7,15 @@ from mmengine.utils.misc import get_object_from_string
 def template_map_fn(example, template):
     conversation = example.get('conversation', [])
     for i, single_turn_conversation in enumerate(conversation):
-        input_text = ''
         input = single_turn_conversation.get('input', '')
-        if input != '' and input is not None:
-            input = template.INSTRUCTION.format(input=input, round=i + 1)
-            input_text += input
-            instruction_postfix = ''
-        else:
-            instruction_postfix = template.INSTRUCTION.split('{input}')[-1]
+        if input is None:
+            input = ''
+        input_text = template.INSTRUCTION.format(input=input, round=i + 1)
         system = single_turn_conversation.get('system', '')
         if system != '' and system is not None:
             system = template.SYSTEM.format(system=system)
             input_text = system + input_text
-        single_turn_conversation['input'] = input_text + instruction_postfix
+        single_turn_conversation['input'] = input_text
     return {'conversation': conversation}
 
 
