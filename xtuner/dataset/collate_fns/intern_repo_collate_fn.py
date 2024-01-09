bfaeb5fe5a1e46bc93669d02bea0d231afc882d1
HIT-cwh <2892770585@qq.com>
2024-01-09 21:21:42 +0800

    add internlm repo dataset and collate_fn

--- xtuner/dataset/collate_fns/intern_repo_collate_fn.py
+++ xtuner/dataset/collate_fns/intern_repo_collate_fn.py
@@ -1,40 +1,57 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import copy
 from typing import Dict, Sequence
 
 import torch
-from torch.nn.utils.rnn import pad_sequence
-
-from xtuner.utils import DEFAULT_PAD_TOKEN_INDEX, IGNORE_INDEX
 
 
-def intern_repo_collate_fn(
-        instances: Sequence[Dict],
-        pad_index: int = DEFAULT_PAD_TOKEN_INDEX,
-        return_hf_format: bool = False) -> Dict[str, torch.Tensor]:
-    input_ids = []
-    labels = []
+def intern_repo_collate_fn(instances: Sequence[Dict],
+                           packed_length: int,
+                           return_hf_format: bool = False,
+                           use_local_attn: bool = True):
+
+    input_ids, labels = [], []
+    if use_local_attn:
+        cumulative_len, indexes = [], []
+        assert len(instances) == 1, (
+            f'If utilizing local attention, the batch size should be'
+            f' set to 1, but got {len(instances)}')
+
     for example in instances:
-        cur_input_ids = torch.tensor(example['input_ids'])
-        cur_labels = copy.deepcopy(cur_input_ids)
-        cur_input_ids = cur_input_ids.abs()
-        cur_labels[cur_labels < 0] = IGNORE_INDEX
-        input_ids.append(cur_input_ids)
-        labels.append(cur_labels)
-    if len(instances) > 1:
-        input_ids = pad_sequence(
-            input_ids, batch_first=True, padding_value=pad_index)
-        labels = pad_sequence(
-            labels, batch_first=True, padding_value=IGNORE_INDEX)
-    else:
-        input_ids = torch.stack(input_ids)
-        labels = torch.stack(labels)
+        assert (len(example['input_ids']) == packed_length), (
+            f'length of a sample should be equal to packed_length, '
+            f"but got {len(example['input_ids'])} and {packed_length})")
+        assert (len(example['labels']) == packed_length), (
+            f'length of a sample should be equal to packed_length, '
+            f"but got {len(example['labels'])} and {packed_length})")
 
-    data_dict = {
-        'input_ids': input_ids,
-        'attention_mask': input_ids.ne(pad_index),
-        'labels': labels
-    }
+        input_ids_per_sample = [abs(w) for w in example['input_ids']]
+        labels_per_sample = [w if w > 0 else -100 for w in example['labels']]
+
+        input_ids.append(torch.LongTensor(input_ids_per_sample))
+        labels.append(torch.LongTensor(labels_per_sample))
+        if use_local_attn:
+            cumulative_len.append(torch.IntTensor(example['cumulative_len']))
+            indexes.append(torch.LongTensor(example['indexes']))
+
+    input_ids = torch.nn.utils.rnn.pad_sequence(input_ids, batch_first=True)
+    labels = torch.nn.utils.rnn.pad_sequence(
+        labels, batch_first=True, padding_value=-100)
+    assert input_ids.shape[1] == packed_length, (input_ids.shape[1],
+                                                 packed_length)
+
+    if use_local_attn:
+        indexes = torch.stack(indexes, dim=0)
+        max_seqlen = (cumulative_len[0][1:] -
+                      cumulative_len[0][:-1]).max().item()
+        data_dict = {
+            'input_ids': input_ids,
+            'cumulative_len': cumulative_len,
+            'indexes': indexes,
+            'labels': labels,
+            'max_seqlen': max_seqlen
+        }
+    else:
+        data_dict = {'input_ids': input_ids, 'labels': labels}
 
     if return_hf_format:
         return data_dict
--- xtuner/dataset/intern_repo.py
+++ xtuner/dataset/intern_repo.py
@@ -1,73 +1,264 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import itertools as it
+import json
+import mmap
+import operator
 import os
+import threading
+from copy import deepcopy
+from pathlib import Path
 
-from datasets import concatenate_datasets, load_dataset
+import numpy as np
+import torch
+from datasets import Dataset
 from mmengine import print_log
 from torch import distributed as dist
+from torch.utils.data import ConcatDataset
 from tqdm import tqdm
 
-from .utils import InternRepoPacker
+
+class JsonlDataset(torch.utils.data.Dataset):
+    """
+
+    JSONL format is expected to roughly follow that of The Pile.
+    One-line-per-document of the form:
+    ```
+    {
+        "tokens": List[int],
+    }
+    ```
+
+    Note that only the "tokens" key is used.
+    """
+
+    def __init__(self, path: str, min_length=50):
+        self.path = path
+        self.threadlocal = threading.local()
+        resolved_path = Path(path).resolve()
+        self.resolved_path = resolved_path
+        self.meta = Path(f'{resolved_path}.meta')
+
+        # only build the cache in on the primary worker to prevent
+        # overloading nfs
+        assert os.path.exists(
+            self.meta
+        ), f'The cache file:{self.meta} is not found for file:{self.path}'
+        try:
+            with open(self.meta, 'rb') as f:
+                meta = np.load(f)
+        except Exception as e:
+            print(f'Cannot load file {self.meta}...')
+            raise e
+        self.offsets = meta[:, 0]
+        self.lengths = meta[:, -1]
+
+        if min_length > 0:
+            mask = self.lengths >= min_length
+            self.old_lengths = self.lengths.copy()
+            self.old_length = len(self.offsets)
+            self.offsets = self.offsets[mask]
+            self.lengths = self.lengths[mask]
+
+    def __getitem__(self, idx):
+        f = self._get_mmap()
+        position = self.offsets[idx]
+        f.seek(position)
+        item = f.readline().decode('utf-8')
+        try:
+            item = json.loads(item)
+            item['length'] = len(item['tokens'])  # add a length info
+        except Exception as err:
+            raise json.decoder.JSONDecodeError(
+                doc=self.path,
+                pos=position,
+                msg=(f'Error while loading JSONL line in file {self.path} '
+                     f'at byte {position}. Contents of line:\n{item}\n{err}'),
+            )
+        return item
+
+    def get_dataset_name(self):
+        return str(self.resolved_path)
+
+    def _get_mmap(self):
+        if not hasattr(self.threadlocal, 'handles'):
+            with open(self.path, 'rb') as f:
+                mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+                self.threadlocal.handles = [f, mm]
+                if self.path.endswith('.gz') or self.path.endswith(
+                        '.bz') or self.path.endswith('.bz2'):
+                    raise NotImplementedError(
+                        'Compressed files are not supported because .seek() '
+                        'would require rereading the entire file, making '
+                        'performance too slow.')
+        return self.threadlocal.handles[-1]
+
+    def __setstate__(self, state):
+        self.__dict__ = state
+        self.threadlocal = threading.local()
+
+    def __getstate__(self):
+        d = {}
+        for i, v in self.__dict__.items():
+            if i != 'threadlocal':
+                d[i] = v
+        return d
+
+    def __del__(self):
+        if hasattr(self.threadlocal, 'handles'):
+            # cleanup files we opened on initialization
+            while self.threadlocal.handles:
+                self.threadlocal.handles.pop().close()
+
+    @staticmethod
+    def exists(path):
+        return os.path.exists(path)
+
+    def __len__(self):
+        # Virtual length of the dataset depends on the epoch number
+        # if the number of documents is not perfectly divisible by the
+        # data_subshard_count
+        return len(self.offsets)
 
 
-def process(dataset_folder,
-            max_length=2048,
-            split='train',
-            shuffle_before_pack=True,
-            pack_to_max_length=False,
-            map_num_proc=32):
+class PackedDataset(torch.utils.data.Dataset):
+    """The class PackedDataset takes in a dataset and aggregates samples of
+    different lengths together based on the packed_length.
 
-    ds = []
-    for root, dirs, files in os.walk(dataset_folder, followlinks=True):
-        for fn in tqdm(sorted(files), total=len(files), leave=False):
+    Args:
+        dataset: The original dataset to pack.
+        packed_length: The length of each packed sample. Default is 8192.
+    """
+
+    def __init__(self, dataset, packed_length: int = 8192, seed: int = 1024):
+        self.dataset = dataset
+        self.packed_length = packed_length
+        if isinstance(dataset, JsonlDataset):
+            self.lengths = dataset.lengths
+        elif isinstance(dataset, Dataset):
+            assert 'lengths' in dataset.column_names
+            self.lengths = dataset['lengths']
+        else:
+            raise NotImplementedError
+        self.seed = seed
+
+        rng = np.random.RandomState(self.seed)
+        shuffled_indices = np.arange(len(self.lengths))
+        rng.shuffle(shuffled_indices)
+        self.shuffled_indices = shuffled_indices
+        self.shuffled_samples_len = list(
+            map(self.lengths.__getitem__, shuffled_indices))
+        self.shuffled_accumulated_samples_len = list(
+            it.accumulate(self.shuffled_samples_len, operator.add))
+        self.num_tokens = sum(self.lengths)
+
+    def __len__(self):
+        return self.num_tokens // self.packed_length
+
+    def search_sample_index(self, pack_idx: int = 0):
+        assert pack_idx >= 0
+        length_train = (pack_idx + 1) * self.packed_length
+        sample_index = np.searchsorted(
+            self.shuffled_accumulated_samples_len, length_train, side='left')
+        return sample_index
+
+    def mapping(self, pack_idx: int = 0):
+        begin_sample_idx, begin_token_id = 0, 0
+        if pack_idx > 0:
+            begin_sample_idx = self.search_sample_index(pack_idx - 1)
+            begin_token_id = self.shuffled_samples_len[begin_sample_idx] - (
+                self.shuffled_accumulated_samples_len[begin_sample_idx] -
+                (pack_idx) * self.packed_length
+            )  # 前一条packed数据结束的位置是那条数据的第几个token
+            if begin_token_id == self.shuffled_samples_len[begin_sample_idx]:
+                begin_sample_idx += 1
+                begin_token_id = 0
+
+        end_sample_idx = self.search_sample_index(pack_idx)
+        end_token_id = self.shuffled_samples_len[end_sample_idx] - (
+            self.shuffled_accumulated_samples_len[end_sample_idx] -
+            (pack_idx + 1) * self.packed_length)
+        return begin_sample_idx, begin_token_id, end_sample_idx, end_token_id
+
+    def build_pack(self, begin_sample_idx: int, begin_token_id: int,
+                   end_sample_idx: int, end_token_id: int):
+        pack, cumulative_len, indexes, labels = [], [0], [], []
+
+        while begin_sample_idx < end_sample_idx:
+            sample_idx = self.shuffled_indices[begin_sample_idx]
+            sample = self.dataset[sample_idx]
+            chunk = sample['tokens'][begin_token_id:]
+            pack.extend(chunk)
+            _labels = deepcopy(chunk)
+            assert len(_labels) == len(chunk), (_labels, chunk)
+            labels.extend(_labels)
+            cumulative_len.append(cumulative_len[-1] + len(chunk))
+            indexes.extend(list(range(len(chunk))))
+            begin_sample_idx = begin_sample_idx + 1
+            begin_token_id = 0
+
+        sample_idx = self.shuffled_indices[end_sample_idx]
+        sample = self.dataset[sample_idx]
+        chunk = sample['tokens'][begin_token_id:
+                                 end_token_id]  # fragment of a sample
+        pack.extend(chunk)
+        _labels = deepcopy(chunk)
+        assert len(_labels) == len(chunk), (_labels, chunk)
+        labels.extend(_labels)
+        cumulative_len.append(cumulative_len[-1] + len(chunk))
+        indexes.extend(list(range(len(chunk))))
+
+        out = {
+            'input_ids': pack,
+            'cumulative_len': cumulative_len,
+            'indexes': indexes,
+            'labels': labels
+        }
+        return out
+
+    def __getitem__(self, item: int):
+        pos_before, token_id_before, pos_after, token_id_after = self.mapping(
+            item)
+        return self.build_pack(pos_before, token_id_before, pos_after,
+                               token_id_after)
+
+
+def process_intern_repo_dataset(folder,
+                                packed_length=8192,
+                                min_length=0,
+                                seed=1024):
+
+    assert os.path.exists(folder), f'{folder} does not exist.'
+    datasets = []
+    if dist.get_rank() == 0:
+        triples = [list(os.walk(folder, followlinks=True))]
+    else:
+        triples = [None]
+    dist.broadcast_object_list(triples, src=0)
+    triples = triples[0]
+
+    for root, dirs, files in triples:
+        dirs.sort()  # Let the folder need to be returned in a fixed order
+        if dist.get_rank() == 0:
+            print_log(f'Reading {root}...', logger='current')
+        num_token_in_folder = 0
+
+        for fn in tqdm(
+                sorted(files),
+                total=len(files),
+                leave=False,
+                disable=dist.get_rank() != 0):
             if fn.endswith('.bin'):
                 fp = os.path.join(root, fn)
-                ds.append(load_dataset('json', data_files=fp)[split])
+                ds = JsonlDataset(fp, min_length=min_length)
 
-    dataset = concatenate_datasets(ds)
-    print_log(f'Find {len(dataset)} samples.', 'current')
-    dataset = dataset.rename_column('tokens', 'input_ids')
+                if len(ds) == 0:
+                    continue
 
-    # pack to max length
-    if pack_to_max_length:
-        if shuffle_before_pack:
-            dataset = dataset.shuffle()
-            dataset = dataset.flatten_indices(num_proc=map_num_proc)
-        dataset = dataset.map(
-            InternRepoPacker(max_length), batched=True, num_proc=map_num_proc)
-        print_log(
-            f'After packing to {max_length}, '
-            f'the length of dataset is {len(dataset)}.', 'current')
+                ds = PackedDataset(ds, packed_length, seed=seed)
+
+                num_token_in_folder += len(ds) * packed_length
+                datasets.append(ds)
+
+    dataset = ConcatDataset(datasets=datasets)
 
     return dataset
-
-
-def process_intern_repo_dataset(*args, **kwargs):
-    """Post-process the dataset in InternLM repo
-    (https://github.com/InternLM/InternLM) format.
-
-    The training dataset of InternLM is pre-tokenized, and is formatted as
-    follows:
-
-    ```
-    {"tokens": [1, -333, -352, -1621, ..., 103028, 13, 2]}
-    {"tokens": [1, -333, -352, -1621, ..., 103028, 13, 2]}
-    ```
-
-    Among them, tokens with negative values are not involved in the calculation
-    of loss during the training process.
-
-    Note:
-        This function is specifically designed for processing data in the
-        internlm format. However, it should not be misconstrued as a tool for
-        training the internlm model.
-    """
-    if not (dist.is_available() and dist.is_initialized()):
-        return process(*args, **kwargs)
-
-    if dist.get_rank() == 0:
-        dataset = process(*args, **kwargs)
-        objects = [dataset]
-    else:
-        objects = [None]
-    dist.broadcast_object_list(objects, src=0)
-    return objects[0]
