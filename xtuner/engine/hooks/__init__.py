1a590193dc639e46b2a17352bb7162785d9edeb7
HIT-cwh <2892770585@qq.com>
2024-01-09 21:16:22 +0800

    add local_attn_args_to_messagehub_hook

--- xtuner/engine/hooks/__init__.py
+++ xtuner/engine/hooks/__init__.py
@@ -1,6 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .dataset_info_hook import DatasetInfoHook
 from .evaluate_chat_hook import EvaluateChatHook
+from .local_attn_args_to_messagehub_hook import LocalAttnArgsToMessageHubHook
 from .throughput_hook import ThroughputHook
 
-__all__ = ['EvaluateChatHook', 'DatasetInfoHook', 'ThroughputHook']
+__all__ = [
+    'EvaluateChatHook', 'DatasetInfoHook', 'ThroughputHook',
+    'LocalAttnArgsToMessageHubHook'
+]
