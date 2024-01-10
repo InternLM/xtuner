0bfbf65b074f22998c7cc771ab71fc9d924606a0
Zhihao Lin <36994684+LZHgrla@users.noreply.github.com>
2024-01-10 17:22:26 +0800

    [Fix] Fix `ConcatDataset` (#298)
    * fix bugs
    
    * update cfgs

--- xtuner/dataset/concat_dataset.py
+++ xtuner/dataset/concat_dataset.py
@@ -6,27 +6,14 @@ from xtuner.registry import BUILDER
 
 class ConcatDataset(TorchConcatDataset):
 
-    def __init__(self, datasets_cfg, datasets_kwargs=None):
-        datasets = []
-        names = []
-        for name, cfg in datasets_cfg.items():
-            if datasets_kwargs is not None:
-                cfg.update(datasets_kwargs)
-            datasets.append(BUILDER.build(cfg))
-            names.append(name)
-        self.names = names
-        super().__init__(datasets=datasets)
+    def __init__(self, datasets):
+        datasets_instance = []
+        for cfg in datasets:
+            datasets_instance.append(BUILDER.build(cfg))
+        super().__init__(datasets=datasets_instance)
 
     def __repr__(self):
         main_str = 'Dataset as a concatenation of multiple datasets. \n'
-        main_str += '\n'.join([
-            f'{name}: {repr(dataset)},'
-            for name, dataset in zip(self.names, self.datasets)
-        ])
+        main_str += ',\n'.join(
+            [f'{repr(dataset)}' for dataset in self.datasets])
         return main_str
-
-    def __getattr__(self, name: str):
-        try:
-            return super().__getattr__(name)
-        except AttributeError:
-            return [getattr(dataset, name) for dataset in self.datasets]
