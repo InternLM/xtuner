55d842482025be3da82f1cc373449229925a50dd
pppppM <gjf_mail@126.com>
2024-01-10 20:44:49 +0800

    update conf.py

--- docs/zh_cn/conf.py
+++ docs/zh_cn/conf.py
@@ -6,14 +6,18 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import os
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
 import pytorch_sphinx_theme
+
+sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
