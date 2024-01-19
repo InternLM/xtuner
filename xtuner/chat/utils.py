2b2881cdd45c45e32e254334f0fad3e0b01fc3fc
pppppM <gjf_mail@126.com>
2024-01-19 21:55:44 +0800

    fix gen config

--- xtuner/chat/utils.py
+++ xtuner/chat/utils.py
@@ -10,5 +10,5 @@ class GenerationConfig():
     top_k: int = field(default=40)
     top_p: float = field(default=0.75)
     repetition_penalty: float = field(default=1.0)
-    stop_words: list = field(default_factory=[])
+    stop_words: list = field(default_factory=lambda: [])
     seed: Optional[int] = field(default=None)
