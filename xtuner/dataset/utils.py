475990bf43dafd846c1bf928b1855f6faa1ba5ff
HIT-cwh <2892770585@qq.com>
2024-01-24 19:52:44 +0800

    modify Packer to support varlen attn

--- xtuner/dataset/utils.py
+++ xtuner/dataset/utils.py
@@ -5,6 +5,7 @@ import io
 from io import BytesIO
 from itertools import chain
 
+import numpy as np
 import requests
 from PIL import Image
 
@@ -114,18 +115,65 @@ def encode_fn(example,
 
 
 class Packer:
-    # modified from
-    # https://github.com/facebookresearch/llama-recipes/blob/main/ft_datasets/utils.py
+    """Only used for packing data in InternLM repo
+    (https://github.com/InternLM/InternLM) format."""
 
-    def __init__(self, chunk_size=2048):
+    def __init__(self,
+                 chunk_size=2048,
+                 use_varlen_attn=False,
+                 drop_last=False):
+        use_varlen_attn = True
+        drop_last = True
         self.chunk_size = chunk_size
         self.residual = {'input_ids': [], 'labels': []}
+        self.use_varlen_attn = use_varlen_attn
+        self.drop_last = drop_last
+        if use_varlen_attn:
+            self.residual_cumulative_len = [0]
+
+    def get_cumulative_len(self, chunk_num):
+        ptr_l = 0
+        cumulative_len = []
+        for chunk_idx in range(chunk_num):
+            length_train = (chunk_idx + 1) * self.chunk_size
+            ptr_r = np.searchsorted(
+                self.residual_cumulative_len, length_train, side='left')
+            if self.residual_cumulative_len[ptr_r] == length_train:
+                cumulative_len_cur = \
+                    self.residual_cumulative_len[ptr_l:ptr_r + 1]
+                ptr_l = ptr_r + 1
+            else:
+                cumulative_len_cur = self.residual_cumulative_len[
+                    ptr_l:ptr_r] + [length_train]
+                ptr_l = ptr_r
+            cumulative_len_cur = [
+                num - chunk_idx * self.chunk_size for num in cumulative_len_cur
+            ]
+            if cumulative_len_cur[0] != 0:
+                cumulative_len_cur = [0] + cumulative_len_cur
+
+            cumulative_len.append(cumulative_len_cur)
+
+        self.residual_cumulative_len = [
+            num - length_train for num in self.residual_cumulative_len[ptr_l:]
+        ]
+        if len(self.residual_cumulative_len) == 0:
+            self.residual_cumulative_len = [0]
+        elif self.residual_cumulative_len[0] != 0:
+            self.residual_cumulative_len = [0] + self.residual_cumulative_len
+
+        return cumulative_len
 
     def __call__(self, batch):
         concatenated_samples = {
             k: v + list(chain(*batch[k]))
             for k, v in self.residual.items()
         }
+
+        if self.use_varlen_attn:
+            for input_id in batch['input_ids']:
+                self.residual_cumulative_len.append(
+                    self.residual_cumulative_len[-1] + len(input_id))
 
         total_length = len(concatenated_samples[list(
             concatenated_samples.keys())[0]])
@@ -134,9 +182,11 @@ class Packer:
             chunk_num = total_length // self.chunk_size
             result = {
                 k: [
-                    v[i:i + self.chunk_size]
-                    for i in range(0, chunk_num *
-                                   self.chunk_size, self.chunk_size)
+                    v[i:i + self.chunk_size] for i in range(
+                        0,
+                        chunk_num *  # noqa: W504
+                        self.chunk_size,
+                        self.chunk_size)
                 ]
                 for k, v in concatenated_samples.items()
             }
@@ -144,39 +194,20 @@ class Packer:
                 k: v[(chunk_num * self.chunk_size):]
                 for k, v in concatenated_samples.items()
             }
+
+            if self.use_varlen_attn:
+                result['cumulative_len'] = self.get_cumulative_len(chunk_num)
         else:
-            result = {k: [v] for k, v in concatenated_samples.items()}
+            if self.drop_last:
+                result = {k: [] for k, v in concatenated_samples.items()}
+            else:
+                result = {k: [v] for k, v in concatenated_samples.items()}
             self.residual = {k: [] for k in concatenated_samples.keys()}
-
-        return result
-
-
-class InternRepoPacker:
-    """Only used for packing data in InternLM repo
-    (https://github.com/InternLM/InternLM) format."""
-
-    def __init__(self, chunk_size=2048):
-        self.chunk_size = chunk_size
-        self.residual = []
-
-    def __call__(self, batch):
-        concatenated_samples = self.residual + list(chain(*batch['input_ids']))
-
-        total_length = len(concatenated_samples)
-
-        if total_length >= self.chunk_size:
-            chunk_num = total_length // self.chunk_size
-            input_ids = [
-                concatenated_samples[i:i + self.chunk_size]
-                for i in range(0, chunk_num * self.chunk_size, self.chunk_size)
-            ]
-            result = {'input_ids': input_ids}
-            self.residual = concatenated_samples[(chunk_num *
-                                                  self.chunk_size):]
-        else:
-            input_ids = [concatenated_samples]
-            result = {'input_ids': input_ids}
-            self.residual = []
+            if self.use_varlen_attn:
+                result['cumulative_len'] = [] if self.drop_last else [
+                    self.residual_cumulative_len
+                ]
+                self.residual_cumulative_len = [0]
 
         return result
 
