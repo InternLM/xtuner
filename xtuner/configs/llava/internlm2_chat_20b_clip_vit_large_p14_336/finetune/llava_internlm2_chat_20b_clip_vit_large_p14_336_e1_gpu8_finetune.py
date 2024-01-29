5951cbc6181808d51ee54e15354a5f0c3b1138ef
Zhihao Lin <36994684+LZHgrla@users.noreply.github.com>
2024-01-29 15:33:21 +0800

    [Fix] Fix `batch_size` of full fine-tuing LLaVA-InternLM2 (#360)
    fix

--- xtuner/configs/llava/internlm2_chat_20b_clip_vit_large_p14_336/finetune/llava_internlm2_chat_20b_clip_vit_large_p14_336_e1_gpu8_finetune.py
+++ xtuner/configs/llava/internlm2_chat_20b_clip_vit_large_p14_336/finetune/llava_internlm2_chat_20b_clip_vit_large_p14_336_e1_gpu8_finetune.py
@@ -33,8 +33,8 @@ prompt_template = PROMPT_TEMPLATE.internlm2_chat
 max_length = int(2048 - (336 / 14)**2)
 
 # Scheduler & Optimizer
-batch_size = 8  # per_device
-accumulative_counts = 2
+batch_size = 4  # per_device
+accumulative_counts = 4
 
 dataloader_num_workers = 0
 max_epochs = 1
