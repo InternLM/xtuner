071ebac0e98e0d045de2ce42f559fb6cd18f3de6
LZHgrla <linzhihao@pjlab.org.cn>
2024-02-02 12:34:47 +0800

    fix

--- xtuner/dataset/map_fns/dataset_map_fns/alpaca_zh_map_fn.py
+++ xtuner/dataset/map_fns/dataset_map_fns/alpaca_zh_map_fn.py
@@ -1,11 +1,9 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from xtuner.utils import SYSTEM_TEMPLATE
 
 
 def alpaca_zh_map_fn(example):
     return {
         'conversation': [{
-            'system': SYSTEM_TEMPLATE.alpaca,
             'input': f"{example['instruction_zh']}\n{example['input_zh']}",
             'output': example['output_zh']
         }]
