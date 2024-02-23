333f70950111f19d28e7bb4a95c2b8e1154ad2f3
PommesPeter <me@pommespeter.space>
2024-02-23 19:51:49 +0800

    added  required version

--- xtuner/configs/gemma/gemma_2b/gemma_2b_full_alpaca_e3.py
+++ xtuner/configs/gemma/gemma_2b/gemma_2b_full_alpaca_e3.py
@@ -20,7 +20,7 @@ from xtuner.utils import PROMPT_TEMPLATE, SYSTEM_TEMPLATE
 #                          PART 1  Settings                           #
 #######################################################################
 # Model
-pretrained_model_name_or_path = 'google/gemma-2b'
+pretrained_model_name_or_path = 'google/gemma-2b' # required transformers>=4.38.1
 use_varlen_attn = False
 
 # Data
