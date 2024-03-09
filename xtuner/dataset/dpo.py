df99211302b2943925ac40b4a19600115fcdc6dc
xiaohangguo <1216762329@qq.com>
2024-03-09 22:54:05 +0800

    [WIP] test dpo __getitem__

--- xtuner/dataset/dpo.py
+++ xtuner/dataset/dpo.py
@@ -12,25 +12,41 @@ from torch.utils.data import Dataset
 from xtuner.registry import BUILDER
 from .huggingface import process_hf_dataset
 from .utils import expand2square
-
+from xtuner.utils import DEFAULT_IMAGE_TOKEN, IGNORE_INDEX, IMAGE_TOKEN_INDEX
 
 class DPODataset(Dataset):
-
-    def __init__(self,
-                 data_path,
-                 tokenizer,
-                 max_dataset_length=None,
-                 dataset_map_fn=None,
-                 template_map_fn=None,
-                 max_length=2048):
-        super().__init__()
-        # TODO
-        pass
+    def __init__(self, dpo_dataset_dict, tokenizer):
+        self.prompt_data = dpo_dataset_dict['prompt']
+        self.chosen_data = dpo_dataset_dict['chosen']
+        self.rejected_data = dpo_dataset_dict['rejected']
+        self.tokenizer = tokenizer
 
     def __len__(self):
-        # TODO
-        pass
+        return len(self.prompt_data)
 
     def __getitem__(self, index):
-        # TODO(lsh) 对于DPO数据集，分词编码返回预处理过的data_dict
-        pass
+        prompt = self.prompt_data[index]
+        chosen = self.chosen_data[index]
+        rejected = self.rejected_data[index]
+
+        # 编码文本
+        prompt_encoding = self.tokenizer(prompt, truncation=True, return_tensors='pt')
+        chosen_encoding = self.tokenizer(chosen, truncation=True, return_tensors='pt')
+        rejected_encoding = self.tokenizer(rejected, truncation=True, return_tensors='pt')
+
+
+        chosen_labels = [IGNORE_INDEX] * len(prompt_encoding['input_ids'][0]) + chosen_encoding['input_ids'][0].tolist()
+        rejected_labels = [IGNORE_INDEX] * len(prompt_encoding['input_ids'][0]) + rejected_encoding['input_ids'][0].tolist()
+
+        # 构造返回的数据字典
+        data_dict = {
+            'input_chosen_ids': torch.cat((prompt_encoding['input_ids'], chosen_encoding['input_ids']), dim=1).squeeze(0),
+            'chosen_labels': torch.tensor(chosen_labels, dtype=torch.long),
+            'input_reject_ids': torch.cat((prompt_encoding['input_ids'], rejected_encoding['input_ids']), dim=1).squeeze(0),
+            'reject_labels': torch.tensor(rejected_labels, dtype=torch.long),
+        }
+
+        return data_dict
+
+
+
---
+++ xtuner/dataset/test_dpo.py
@@ -0,0 +1,39 @@
+import pytest
+from .dpo import DPODataset
+
+@pytest.fixture
+def dpo_dataset():
+    dpo_dataset_dict = {
+        "prompt": [
+            "hello",
+            "how are you",
+            "What is your name?",
+        ],
+        "chosen": [
+            "hi nice to meet you",
+            "I am fine",
+            "My name is Mary",
+        ],
+        "rejected": [
+            "leave me alone",
+            "I am not fine",
+            "Whats it to you?",
+        ],
+    }
+    
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+    tokenizer = AutoTokenizer.from_pretrained("/public/home/lvshuhang/model_space/workspace/internlm_internlm2-chat-1_8b", trust_remote_code=True)
+    dataset = DPODataset(dpo_dataset_dict, tokenizer)
+    return dataset
+
+# 测试 DPODataset 的长度
+def test_dpo_dataset_length(dpo_dataset):
+    assert len(dpo_dataset) == 3
+
+# 测试 __getitem__ 方法
+def test_dpo_dataset_getitem(dpo_dataset):
+    sample = dpo_dataset[0]
+    print(f"{sample}")
+    # 检查返回字典的键
+    assert all(key in sample for key in ["input_chosen_ids", "chosen_labels", "input_reject_ids", "reject_labels"])
+    
