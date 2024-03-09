d121ca824c693f6666d39923b7df993411883798
xiaohangguo <1216762329@qq.com>
2024-03-09 23:16:47 +0800

    xx

--- xtuner/dataset/test_dpo.py
+++ xtuner/dataset/test_dpo.py
@@ -26,6 +26,20 @@ def dpo_dataset():
     dataset = DPODataset(dpo_dataset_dict, tokenizer)
     return dataset
 
+@pytest.fixture
+def dpo_dataset1():
+    '单条样本结构'
+    dpo_dataset_dict = {
+        "prompt": 
+            "What is your name?",
+        "chosen":  "My name is Mary",
+        "rejected":  "Whats it to you?"
+    }
+    
+    from transformers import AutoTokenizer, AutoModelForCausalLM
+    tokenizer = AutoTokenizer.from_pretrained("/public/home/lvshuhang/model_space/workspace/internlm_internlm2-chat-1_8b", trust_remote_code=True)
+    dataset = DPODataset(dpo_dataset_dict, tokenizer)
+    return dataset
 # 测试 DPODataset 的长度
 def test_dpo_dataset_length(dpo_dataset):
     assert len(dpo_dataset) == 3
@@ -36,4 +50,10 @@ def test_dpo_dataset_getitem(dpo_dataset):
     print(f"{sample}")
     # 检查返回字典的键
     assert all(key in sample for key in ["input_chosen_ids", "chosen_labels", "input_reject_ids", "reject_labels"])
+
     
+def test_dpo_dataset_getitem1(dpo_dataset1):
+    sample = dpo_dataset1[0]
+    print(f"{sample}")
+    # 检查返回字典的键
+    assert all(key in sample for key in ["input_chosen_ids", "chosen_labels", "input_reject_ids", "reject_labels"])    
