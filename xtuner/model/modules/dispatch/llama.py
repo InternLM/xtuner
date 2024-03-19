3b24388aa054fa1f7c0c825e986e9ea6adf388db
HIT-cwh <2892770585@qq.com>
2024-03-19 16:21:57 +0800

    fix apply_rotary_pos_emb bug

--- xtuner/model/modules/dispatch/llama.py
+++ xtuner/model/modules/dispatch/llama.py
@@ -6,6 +6,7 @@ import torch
 import torch.distributed as dist
 import torch.nn.functional as F
 from mmengine import MessageHub
+from transformers.models.llama.modeling_llama import apply_rotary_pos_emb
 from transformers.utils import logging
 
 from xtuner.parallel.sequence import sequence_parallel_wrapper
@@ -37,18 +38,6 @@ def rotate_half(x):
     x1 = x[..., :x.shape[-1] // 2]
     x2 = x[..., x.shape[-1] // 2:]
     return torch.cat((-x2, x1), dim=-1)
-
-
-def apply_rotary_pos_emb(q, k, cos, sin, position_ids):
-    # The first two dimensions of cos and sin are always 1,
-    # so we can `squeeze` them.
-    cos = cos.squeeze(1).squeeze(0)  # [seq_len, dim]
-    sin = sin.squeeze(1).squeeze(0)  # [seq_len, dim]
-    cos = cos[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
-    sin = sin[position_ids].unsqueeze(1)  # [bs, 1, seq_len, dim]
-    q_embed = (q * cos) + (rotate_half(q) * sin)
-    k_embed = (k * cos) + (rotate_half(k) * sin)
-    return q_embed, k_embed
 
 
 def repeat_kv(hidden_states: torch.Tensor, n_rep: int) -> torch.Tensor:
