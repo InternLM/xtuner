7edb76d30c083a93f2e9eb7772d361cb5e6e7284
pppppM <gjf_mail@126.com>
2024-03-29 19:08:39 +0800

    remove encoder

--- xtuner/chat/backend/__init__.py
+++ xtuner/chat/backend/__init__.py
@@ -1,5 +1,4 @@
-from .encoder import VisionEncoderForDeploy
 from .huggingface import HFBackend
 from .lmdeploy import LMDeployBackend
 
-__all__ = ['VisionEncoderForDeploy', 'HFBackend', 'LMDeployBackend']
+__all__ = ['HFBackend', 'LMDeployBackend']
