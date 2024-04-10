d0c43e6392fa4b519499d453123de001af3b2c23
LZHgrla <linzhihao@pjlab.org.cn>
2024-04-10 14:06:34 +0800

    sphinx-rtd-theme

--- docs/zh_cn/conf.py
+++ docs/zh_cn/conf.py
@@ -68,7 +68,7 @@ language = 'zh_CN'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = 'sphinx_rtd_theme'
 html_logo = '_static/image/logo.png'
 html_theme_options = {
     'path_to_docs': 'docs/zh_cn',
