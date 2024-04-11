040375e53abc3a9a72a9c96cfbd193ec3d341358
HIT-cwh <2892770585@qq.com>
2024-04-11 13:21:05 +0800

    refine

--- xtuner/parallel/sequence/attention.py
+++ xtuner/parallel/sequence/attention.py
@@ -6,8 +6,11 @@ from .setup_distributed import (get_sequence_parallel_group,
                                 get_sequence_parallel_world_size)
 
 
-def pre_process_for_sequence_parallel_attn(query_states, key_states,
-                                           value_states):
+def pre_process_for_sequence_parallel_attn(query_states,
+                                           key_states,
+                                           value_states,
+                                           scatter_dim=2,
+                                           gather_dim=1):
     sequence_parallel_world_size = get_sequence_parallel_world_size()
     n_head = query_states.shape[2]
     assert n_head % sequence_parallel_world_size == 0, \
@@ -17,17 +20,35 @@ def pre_process_for_sequence_parallel_attn(query_states, key_states,
 
     # (b, s // sp_world_size, nd, dim) -> (b, s, nd // sp_world_size, dim)
     sequence_parallel_group = get_sequence_parallel_group()
-    query_states = all_to_all(query_states, sequence_parallel_group, 2, 1)
-    key_states = all_to_all(key_states, sequence_parallel_group, 2, 1)
-    value_states = all_to_all(value_states, sequence_parallel_group, 2, 1)
+    query_states = all_to_all(
+        query_states,
+        sequence_parallel_group,
+        scatter_dim=scatter_dim,
+        gather_dim=gather_dim)
+    key_states = all_to_all(
+        key_states,
+        sequence_parallel_group,
+        scatter_dim=scatter_dim,
+        gather_dim=gather_dim)
+    value_states = all_to_all(
+        value_states,
+        sequence_parallel_group,
+        scatter_dim=scatter_dim,
+        gather_dim=gather_dim)
 
     return query_states, key_states, value_states
 
 
-def post_process_for_sequence_parallel_attn(attn_output):
+def post_process_for_sequence_parallel_attn(attn_output,
+                                            scatter_dim=1,
+                                            gather_dim=2):
     # (b, s, nd // sp_world_size, dim) -> (b, s // sp_world_size, nd, dim)
     sequence_parallel_group = get_sequence_parallel_group()
-    output = all_to_all(attn_output, sequence_parallel_group, 1, 2)
+    output = all_to_all(
+        attn_output,
+        sequence_parallel_group,
+        scatter_dim=scatter_dim,
+        gather_dim=gather_dim)
     return output
 
 
