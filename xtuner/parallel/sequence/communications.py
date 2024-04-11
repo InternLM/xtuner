0c336cd07206ad1cb26cb8a629a078d9fc697f5f
HIT-cwh <2892770585@qq.com>
2024-04-11 12:09:01 +0800

    add explanation about why grad_scale is needed

--- xtuner/parallel/sequence/communications.py
+++ xtuner/parallel/sequence/communications.py
@@ -119,6 +119,54 @@ class _GatherForwardSplitBackward(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
+        """Why we need grad scale? Here is a simple case.
+
+        -------- SP 1 -----------
+        Suppose here is a toy model with only one linear module
+        (in_features = 2, out_features = 1) and the input x has shape(2, 2).
+        Y = [[y1], = [[w11x11 + w21x12], = [[x11, x12], dot [[w11],
+             [y2]]    [w11x21 + w21x22]]    [x21, x22]]      [w21]]
+        z = sum(Y) = y1 + y2
+        Here is the partial derivative of z with respect to w11:
+        ∂z / ∂w11 = ∂z / ∂y1 * ∂y1 / ∂w11 + ∂z / ∂y2 * ∂y2 / ∂w11
+                  = 1 * x11 + 1 * x21 = x11 + x21
+
+        -------- SP 2 -----------
+        When sequence parallel world size is set to 2, we will split the input x
+        and scatter them to the two rank in the same sequence parallel group.
+        ```Step 1
+        Y_rank0 = [[y1]] = [[w11x11 + w21x12]] = [[x11, x12]] dot [[w11, w21]]^T
+        Y_rank1 = [[y2]] = [[w11x21 + w21x22]] = [[x21, x22]] dot [[w11, w21]]^T
+        ```
+
+        Then, we have to gather them:
+        ```Step 2
+        Y_rank0 = [[y1],
+                   detach([y2])]
+        Y_rank1 = [detach([y1]),
+                   [y2]]
+        ```
+        Note that y2 in Y_rank0 does not have grad, neither does y1 in Y_rank1.
+
+        Similarly, we calculate the loss in each rank:
+        ```Step 3
+        z_rank0 = sum(Y_rank0) = y1 + detach(y2)
+        z_rank1 = sum(Y_rank1) = detach(y1) + y2
+        ```
+        So the partial derivative of loss_rank0 with respect to w11:
+        ```∂z / ∂w11 = ∂z / ∂y1 * ∂y1 / ∂w11 = x11```
+        The same for rank1:
+        ```∂z / ∂w11 = ∂z / ∂y2 * ∂y2 / ∂w11 = x21```
+
+        Finally, we need to all_reduce them:
+        ```Step 4
+        In both rank:
+        ∂z / ∂w11 = (x11 + x21) / 2
+        ```
+
+        In SP2, the gradient of each param is only half of that in SP1.
+        So we should scale up the grad during the backward process in Step 2.
+        """  # noqa: E501
         if ctx.grad_scale == 'up':
             grad_output = grad_output * dist.get_world_size(ctx.sp_group)
         elif ctx.grad_scale == 'down':
