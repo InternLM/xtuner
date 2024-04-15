aad9ee379d49e5891fe30f37dbadfd4696b4ec39
pppppM <gjf_mail@126.com>
2024-04-15 19:53:08 +0800

    remove useless code

--- xtuner/dataset/train/__init__.py
+++ xtuner/dataset/train/__init__.py
@@ -1,12 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .base import BaseTrainDataset
-from .mappings import map_protocol, map_sequential, openai_to_raw_training
 from .text import TextTrainDataset
 
 __all__ = [
     'BaseTrainDataset',
     'TextTrainDataset',
-    'map_protocol',
-    'map_sequential',
-    'openai_to_raw_training',
 ]
