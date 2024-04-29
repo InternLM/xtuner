ff61776e38b8116b56f4ae5c5d8f268b72bbf2f4
HIT-cwh <2892770585@qq.com>
2024-04-29 16:10:28 +0800

    delete useless codes

--- xtuner/engine/hooks/hf_checkpoint_hook.py
+++ xtuner/engine/hooks/hf_checkpoint_hook.py
@@ -19,36 +19,6 @@ class HFCheckpointHook(Hook):
     def __init__(self, out_dir: Optional[Union[str, Path]] = None) -> None:
         self.out_dir = out_dir
 
-    @staticmethod
-    def _get_deepspeed_z3_state_dict(model):
-        import deepspeed
-
-        def get_state_dict(module, state_dict, prefix=''):
-            for name, param in module.named_parameters(
-                    prefix=prefix[:-1], recurse=False):
-                gather_param = None
-                with deepspeed.zero.GatheredParameters(param, modifier_rank=0):
-                    if dist.get_rank() == 0:
-                        gather_param = param.to(device='cpu')
-                    else:
-                        gather_param = param.to(device='meta')
-                state_dict[name] = gather_param
-            for name, buffer in module.named_buffers(
-                    prefix=prefix[:-1], recurse=False):
-                if dist.get_rank() == 0:
-                    buffer = buffer.to(device='cpu')
-                else:
-                    buffer = buffer.to(device='meta')
-                state_dict[name] = buffer
-
-            for name, child in module._modules.items():
-                if child is not None:
-                    get_state_dict(child, state_dict, prefix + name + '.')
-
-        state_dict = {}
-        get_state_dict(model, state_dict)
-        return state_dict
-
     def after_run(self, runner) -> None:
         assert isinstance(runner,
                           FlexibleRunner), 'Runner should be `FlexibleRunner`'
