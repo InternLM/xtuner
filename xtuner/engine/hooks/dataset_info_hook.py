eb647cab41c2df2832f0620aa1c42a47b99e14a8
Haian Huang(深度眸) <huanghaian@sensetime.com>
2024-05-10 23:29:41 +0800

    Remove test info in DatasetInfoHook (#622)
    remove test info in DatasetInfoHook

--- xtuner/engine/hooks/dataset_info_hook.py
+++ xtuner/engine/hooks/dataset_info_hook.py
@@ -42,16 +42,12 @@ class DatasetInfoHook(Hook):
     def before_train(self, runner) -> None:
         do_train = runner.train_loop is not None
         do_eval = runner.val_loop is not None
-        do_test = runner.test_loop is not None
         if do_train:
             train_dataset = runner.train_dataloader.dataset
             self.log(runner, train_dataset, mode='train')
         if do_eval:
             eval_dataset = runner.val_dataloader.dataset
             self.log(runner, eval_dataset, mode='eval')
-        if do_test:
-            test_dataset = runner.test_dataloader.dataset
-            self.log(runner, test_dataset, mode='test')
 
     def before_val(self, runner) -> None:
         eval_dataset = runner.val_dataloader.dataset
