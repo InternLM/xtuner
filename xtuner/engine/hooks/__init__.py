fc419431f303aa19abe14226f0e003545d84dd35
whcao <41630003+HIT-cwh@users.noreply.github.com>
2024-05-10 23:28:17 +0800

    [Feature] add HFCheckpointHook to auto save hf model after the whole training phase (#621)
    * add HFCheckpointHook to auto save hf model after the whole training phase
    
    * refinie HFCheckpointHook
    
    * fix lint
    
    * delete useless codes
    
    * fix bugs
    
    * support non-dist training

--- xtuner/engine/hooks/__init__.py
+++ xtuner/engine/hooks/__init__.py
@@ -1,10 +1,11 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .dataset_info_hook import DatasetInfoHook
 from .evaluate_chat_hook import EvaluateChatHook
+from .hf_checkpoint_hook import HFCheckpointHook
 from .throughput_hook import ThroughputHook
 from .varlen_attn_args_to_messagehub_hook import VarlenAttnArgsToMessageHubHook
 
 __all__ = [
     'EvaluateChatHook', 'DatasetInfoHook', 'ThroughputHook',
-    'VarlenAttnArgsToMessageHubHook'
+    'VarlenAttnArgsToMessageHubHook', 'HFCheckpointHook'
 ]
