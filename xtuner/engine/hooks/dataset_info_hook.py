5403c2796259c8f22ead95de2b99027c35903f8d
Xu Song <xusong.vip@gmail.com>
2024-07-11 17:21:07 +0800

    [Feature] Support the DatasetInfoHook of DPO training (#787)
    * [Feature] Support the DatasetInfoHook of DPO training
    
    * fix yapf check

--- xtuner/engine/hooks/dataset_info_hook.py
+++ xtuner/engine/hooks/dataset_info_hook.py
@@ -25,19 +25,26 @@ class DatasetInfoHook(Hook):
         self.is_intern_repo_dataset = is_intern_repo_dataset
 
     def log(self, runner, dataset, mode='train'):
+
+        def _log(input_ids, log_prefix=''):
+            if self.is_intern_repo_dataset:
+                input_ids = [abs(x) for x in input_ids]
+            # Try to split list to be compatible with IMAGE token
+            input_ids = split_list(input_ids, IMAGE_TOKEN_INDEX)
+            text = log_prefix
+            for idx, ids in enumerate(input_ids):
+                text += self.tokenizer.decode(ids)
+                if idx != len(input_ids) - 1:
+                    text += DEFAULT_IMAGE_TOKEN
+            runner.logger.info(text)
+
         runner.logger.info(f'Num {mode} samples {len(dataset)}')
         runner.logger.info(f'{mode} example:')
-        input_ids = dataset[0]['input_ids']
-        if self.is_intern_repo_dataset:
-            input_ids = [abs(x) for x in input_ids]
-        # Try to split list to be compatible with IMAGE token
-        input_ids = split_list(input_ids, IMAGE_TOKEN_INDEX)
-        text = ''
-        for idx, ids in enumerate(input_ids):
-            text += self.tokenizer.decode(ids)
-            if idx != len(input_ids) - 1:
-                text += DEFAULT_IMAGE_TOKEN
-        runner.logger.info(text)
+        if 'chosen_ids' in dataset[0]:
+            _log(dataset[0]['chosen_ids'], log_prefix='chosen: ')
+            _log(dataset[0]['rejected_ids'], log_prefix='rejected: ')
+        else:
+            _log(dataset[0]['input_ids'])
 
     def before_train(self, runner) -> None:
         do_train = runner.train_loop is not None
