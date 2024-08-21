440c9e72f0f61d5de38cf96fb5fc2ce38f50c23c
zhangfeizhao <zhangfeizhao@sensetime.com>
2024-08-21 15:45:35 +0800

    Add data preprocess stastics

---
+++ xtuner/tools/data_preprocess_stastics.sh
@@ -0,0 +1,7 @@
+ROOT=/path/to/xtuner
+export PYTHONPATH=$ROOT:$PYTHONPATH
+
+export OMP_NUM_THREADS=1
+
+
+python data_preprocess_stastics.py --json_file $1 --token_lengths_path $2 --output_path $3 2>&1 | tee -a log_statistics.txt
