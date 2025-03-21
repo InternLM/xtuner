afb5a63c395b5c86a8adcdb001f7dc73d865fbc6
Mashiro <57566630+HAOCHENYE@users.noreply.github.com>
2025-03-21 14:04:46 +0800

    [Fix] Remove `LoadWoInit` in xtuner._lite (#1010)

--- xtuner/_lite/algorithms/ppo/model.py
+++ xtuner/_lite/algorithms/ppo/model.py
@@ -6,8 +6,6 @@ from transformers.utils.import_utils import (
     is_torch_sdpa_available,
 )
 
-from xtuner._lite.accelerate import LoadWoInit
-
 
 def build_actor_model(model_path, dtype=torch.float32, trust_remote_code=True):
     config = AutoConfig.from_pretrained(model_path, trust_remote_code=True)
@@ -16,13 +14,12 @@ def build_actor_model(model_path, dtype=torch.float32, trust_remote_code=True):
     elif is_torch_sdpa_available():
         config.attn_implementation = "sdpa"
 
-    with LoadWoInit():
-        policy = AutoModelForCausalLM.from_pretrained(
-            model_path,
-            attn_implementation="flash_attention_2",
-            torch_dtype=dtype,
-            trust_remote_code=trust_remote_code,
-        )
+    policy = AutoModelForCausalLM.from_pretrained(
+        model_path,
+        attn_implementation="flash_attention_2",
+        torch_dtype=dtype,
+        trust_remote_code=trust_remote_code,
+    )
 
     return policy
 
@@ -36,13 +33,12 @@ def build_reward_model(model_path, dtype=torch.float32, trust_remote_code=True):
 
     config.use_cache = False
     config.torch_dtype = dtype
-    with LoadWoInit():
-        reward = AutoModel.from_pretrained(
-            model_path,
-            attn_implementation="flash_attention_2",
-            torch_dtype=dtype,
-            trust_remote_code=trust_remote_code,
-        )
+    reward = AutoModel.from_pretrained(
+        model_path,
+        attn_implementation="flash_attention_2",
+        torch_dtype=dtype,
+        trust_remote_code=trust_remote_code,
+    )
 
     reward.model.use_cache = False
 
