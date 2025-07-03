0ec0394cbb61a4d9fe1a9118c49935cbf2d95409
HAOCHENYE <21724054@zju.edu.cn>
2025-07-03 04:18:40 +0000

    [Refactor] Add logger

---
+++ xtuner/v1/utils/logger.py
@@ -0,0 +1,29 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import sys
+
+from loguru import logger
+
+
+_LOGGER = None
+
+
+def log_format(debug=False):
+    formatter = "[XTuner][{time:YYYY-MM-DD HH:mm:ss}][<level>{level}</level>]"
+
+    if debug:
+        formatter += "[<cyan>{name}</cyan>:"
+        formatter += "<cyan>{function}</cyan>:"
+        formatter += "<cyan>{line}</cyan>]"
+
+    formatter += " <level>{message}</level>"
+    return formatter
+
+
+def get_logger(level="INFO"):
+    global _LOGGER
+    if _LOGGER is None:
+        # Remove the original logger in Python to prevent duplicate printing.
+        logger.remove()
+        logger.add(sys.stderr, level=level, format=log_format(debug=level == "DEBUG"))
+        _LOGGER = logger
+    return _LOGGER
