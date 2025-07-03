4ae9644bd354549997f50460abe8f38799c1edcc
HAOCHENYE <21724054@zju.edu.cn>
2025-07-03 18:22:40 +0000

    [Refactor] Update training/decoding/prefilling state

---
+++ xtuner/v1/utils/enum_helper.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+
+class StrEnum(str, Enum):
+    """Enum where members are also (and must be) strings."""
+
+    def __new__(cls, *values):
+        """Values must already be of type `str`"""
+        if len(values) > 3:
+            raise TypeError(f"too many arguments for str(): {values!r}")
+        if len(values) == 1:
+            # it must be a string
+            if not isinstance(values[0], str):
+                raise TypeError(f"{values[0]!r} is not a string")
+        if len(values) >= 2:
+            # check that encoding argument is a string
+            if not isinstance(values[1], str):
+                raise TypeError(f"encoding must be a string, not {values[1]!r}")
+        if len(values) == 3:
+            # check that errors argument is a string
+            if not isinstance(values[2], str):
+                raise TypeError("errors must be a string, not %r" % (values[2]))
+        value = str(*values)
+        member = str.__new__(cls, value)
+        member._value_ = value
+        return member
