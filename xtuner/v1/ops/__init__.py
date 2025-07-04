5d343e8b0992a5d682739c490a2d110fa5d0ee68
HAOCHENYE <21724054@zju.edu.cn>
2025-07-04 14:04:53 +0000

    [Refactor] Add `grouped_gemm`

--- xtuner/v1/ops/__init__.py
+++ xtuner/v1/ops/__init__.py
@@ -4,15 +4,16 @@ from .comm import all_to_all_single_autograd, ulysses_all_to_all
 __all__ = ["all_to_all_single_autograd", "ulysses_all_to_all"]
 
 try:
+    from .moe_gemm import moe_grouped_gemm as grouped_gemm
     from .moe_permute import permute, unpermute
 except ImportError:
     ...
 else:
-    __all__.extend(["permute", "unpermute"])
+    __all__.extend(["permute", "unpermute", "grouped_gemm"])
 
 
 def __getattr__(name: str):
-    if name in ["permute", "unpermute"]:
+    if name in ["permute", "unpermute", "grouped_gemm"]:
         # TODO: (yehaochen) replace install url
         raise ImportError(
             f"{name} is not available, please install `grouped_gemm` from https://github.com/fanshiqing/grouped_gemm"
