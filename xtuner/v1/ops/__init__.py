0c87156c866e5c1313e27277ced6878841f8e657
HAOCHENYE <21724054@zju.edu.cn>
2025-07-06 14:37:19 +0000

    [Refactor] Update deep_ep base op
    This is a temp commit. The interface of op will be modified later

--- xtuner/v1/ops/__init__.py
+++ xtuner/v1/ops/__init__.py
@@ -12,11 +12,37 @@ else:
     __all__.extend(["permute", "unpermute", "grouped_gemm"])
 
 
+try:
+    from .comm.deepep_op import (
+        buffer_capture,
+        deep_ep_combine,
+        deep_ep_dispatch,
+        get_deepep_buffer,
+        get_low_latency_buffer,
+    )
+except ImportError:
+    ...
+else:
+    __all__.extend(
+        ["deep_ep_combine", "deep_ep_dispatch", "get_deepep_buffer", "get_low_latency_buffer", "buffer_capture"]
+    )
+
+
 def __getattr__(name: str):
     if name in ["permute", "unpermute", "grouped_gemm"]:
         # TODO: (yehaochen) replace install url
         raise ImportError(
             f"{name} is not available, please install `grouped_gemm` from https://github.com/fanshiqing/grouped_gemm"
+        )
+    elif name in [
+        "deep_ep_dispatch",
+        "deep_ep_combine",
+        "get_low_latency_buffer",
+        "get_deepep_buffer",
+        "buffer_capture",
+    ]:
+        raise ImportError(
+            f"{name} is not available, please install `deep_ep` from https://github.com/deepseek-ai/DeepEP"
         )
     else:
         raise AttributeError(f"module {__name__} has no attribute {name}")
