725f7d57f07439a5567e6dc5393e62841937686e
HAOCHENYE <21724054@zju.edu.cn>
2025-07-07 19:11:34 +0000

    [Refacotr] Add MLA and MHA
    Implement `MultiHeadAttention` and `MultiLatentAttention`

--- xtuner/v1/module/__init__.py
+++ xtuner/v1/module/__init__.py
@@ -1,6 +1,18 @@
+from .attention import (
+    MHAConfig,
+    MLAConfig,
+    MultiHeadAttention,
+    MultiLatentAttention,
+    build_attnention,
+)
 from .rms_norm import RMSNorm
 
 
 __all__ = [
     "RMSNorm",
+    "MultiHeadAttention",
+    "MultiLatentAttention",
+    "MHAConfig",
+    "MLAConfig",
+    "build_attnention",
 ]
---
+++ xtuner/v1/module/attention/__init__.py
@@ -0,0 +1,65 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from typing import Mapping, Protocol, Type
+
+import torch
+
+from xtuner.v1.config import TransformerConfig
+from xtuner.v1.data_proto import SequenceContext
+from xtuner.v1.utils import State
+
+from .mha import MHAConfig, MultiHeadAttention
+from .mla import MLAConfig, MultiLatentAttention
+
+
+class AttentionProtocol(Protocol):
+    """Protocol for attention modules."""
+
+    def __init__(
+        self,
+        config: TransformerConfig,
+        layer_idx: int = 0,
+    ): ...
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        position_embeddings: tuple[torch.Tensor, torch.Tensor],
+        seq_ctx: SequenceContext,
+        past_key_values: list[list[torch.Tensor]] | None = None,
+        state: State = State.TRAINING,
+    ) -> torch.Tensor:
+        """Forward pass of the attention module."""
+        ...
+
+    def __call__(
+        self,
+        hidden_states: torch.Tensor,
+        position_embeddings: tuple[torch.Tensor, torch.Tensor],
+        seq_ctx: SequenceContext,
+        past_key_values: list[list[torch.Tensor]] | None = None,
+        state: State = State.TRAINING,
+    ) -> torch.Tensor:
+        """Forward pass of the attention module."""
+        ...
+
+
+def build_attnention(
+    config: TransformerConfig,
+    layer_idx: int = 0,
+) -> AttentionProtocol:
+    """Build attention module based on the configuration."""
+    if isinstance(config.attention, MHAConfig):
+        return MultiHeadAttention(config, layer_idx)
+    elif isinstance(config.attention, MLAConfig):
+        return MultiLatentAttention(config, layer_idx)
+    else:
+        raise ValueError(f"Unsupported attention type: {type(config.attention)}")
+
+
+__all__ = [
+    "MultiLatentAttention",
+    "MultiHeadAttention",
+    "MHAConfig",
+    "MLAConfig",
+    "AttentionProtocol",
+]
