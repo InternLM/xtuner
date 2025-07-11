f93dd61964d42cc67c7dc29d18ed82f18c4cb925
huanghaian <huanghaian@sensetime.com>
2025-07-11 12:19:10 +0800

    add packing and sampler

---
+++ xtuner/v1/datasets/packing.py
@@ -0,0 +1,246 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import random
+from datasets import concatenate_datasets
+from torch.utils.data import ConcatDataset
+import os
+import numpy as np
+from datasets import Dataset
+from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
+from multiprocessing import shared_memory
+from functools import partial
+from tqdm import tqdm
+import torch
+from xtuner.v1.utils import get_logger
+
+logger = get_logger()
+
+
+class SoftPackDataset(torch.utils.data.Dataset):
+    def __init__(self, datasets, target=2048, blend=False, seed=None):
+        self.seed = seed
+        if blend:
+            num_tokens = [np.concatenate([dset.num_tokens for dset in datasets])]
+            datasets = [ConcatDataset(datasets)]
+        else:
+            num_tokens = [dset.num_tokens for dset in datasets]
+        self.datasets = datasets
+        self.target = target
+
+        pack_infos = []
+        for i, dataset in enumerate(self.datasets):
+            _infos = self.get_pack_infos(dataset, i, num_tokens[i])
+            pack_infos.append(_infos)
+        self.pack_infos = concatenate_datasets(pack_infos)
+
+    @property
+    def longest(self):
+        return self.pack_infos["longest"]
+
+    def get_pack_infos(self, dataset, dataset_id, num_tokens):
+        # _ori_lens = dataset['num_tokens']
+        inds = [i for i in range(len(dataset))]
+        if self.seed is not None:
+            torch.manual_seed(self.seed)
+            random.seed(self.seed)
+        random.shuffle(inds)
+
+        item_buffer = []
+        length_buffer = []
+        longest = 0
+
+        pack_infos = []
+        for shfl_i in inds:
+            if num_tokens[shfl_i] + sum(length_buffer) <= self.target:
+                item_buffer.append(shfl_i)
+                length_buffer.append(num_tokens[shfl_i])
+                longest = max(longest, num_tokens[shfl_i])
+            else:
+                if len(item_buffer) > 0:
+                    info = {
+                        "dataset_id": dataset_id,
+                        "indices": item_buffer,
+                        "longest": int(longest),
+                    }
+                    pack_infos.append(info)
+
+                item_buffer = [shfl_i]
+                length_buffer = [num_tokens[shfl_i]]
+                longest = num_tokens[shfl_i]
+
+        if len(item_buffer) > 0:
+            info = {
+                "dataset_id": dataset_id,
+                "indices": item_buffer,
+                "longest": int(longest),
+            }
+
+            pack_infos.append(info)
+
+        pack_infos = Dataset.from_list(pack_infos)
+
+        return pack_infos
+
+    def __len__(self):
+        return len(self.pack_infos)
+
+    def __getitem__(self, item):
+        indices = self.pack_infos[item]["indices"]
+        dataset_id = self.pack_infos[item]["dataset_id"]
+        return [self.datasets[dataset_id][i] for i in indices]
+
+
+def closest_sum_indices(buffer, value):
+    buffer = np.array(buffer)
+    sorted_indices = np.argsort(buffer)
+    closest_sum = 0
+    closest_indices = []
+
+    for idx in sorted_indices:
+        closest_sum += buffer[idx]
+        if closest_sum <= value:
+            closest_indices.append(int(idx))
+        if closest_sum >= value:
+            break
+
+    return closest_indices
+
+
+def get_pack_chunk_infos(inds, dataset_id, target, flash_attn_block_size, pack_len_type,
+                         pack_extra_buffer_size, num_tokens=None, shm_name=None, shape=None, dtype=None):
+
+    if num_tokens is None:
+        existing_shm = shared_memory.SharedMemory(name=shm_name)
+        num_tokens = np.ndarray(shape, dtype=dtype, buffer=existing_shm.buf)
+
+    item_buffer = []
+    length_buffer = []
+    longest = 0
+    num_patch = 0
+
+    pack_infos = []
+
+    while len(inds) > 0:
+        shfl_i = inds.pop()
+
+        if num_tokens[shfl_i] + sum(length_buffer) <= target:
+            item_buffer.append(shfl_i)
+            length_buffer.append(num_tokens[shfl_i])
+            num_patch += (round(num_tokens[shfl_i] / flash_attn_block_size)) ** 2
+            longest = max(longest, num_tokens[shfl_i])
+        else:
+            if len(item_buffer) > 0:
+                if sum(length_buffer) == target:
+                    info = {
+                        "dataset_id": dataset_id,
+                        "indices": item_buffer,
+                    }
+                    if pack_len_type == "total_block":
+                        info["longest"] = int(num_patch)
+                    elif pack_len_type == "max_block":
+                        info["longest"] = int(longest)
+                    pack_infos.append(info)
+                else:
+                    if pack_extra_buffer_size > 0:
+                        # Try to find the most suitable.
+                        buffer_index = inds[-pack_extra_buffer_size:]
+                        buffer = num_tokens[buffer_index]
+                        closest_indices = closest_sum_indices(buffer, target - sum(length_buffer))
+                        indices_to_remove = []
+                        for closest_inds in closest_indices:
+                            indices_to_remove.append(closest_inds + len(inds) - len(buffer_index))
+                            item_buffer.append(buffer_index[closest_inds])
+                            length_buffer.append(num_tokens[buffer_index[closest_inds]])
+                            num_patch += (
+                                             round(num_tokens[
+                                                       buffer_index[closest_inds]] / flash_attn_block_size)
+                                         ) ** 2
+                            longest = max(longest, num_tokens[buffer_index[closest_inds]])
+
+                        indices_to_remove = sorted(indices_to_remove, reverse=True)
+                        for index in indices_to_remove:
+                            inds.pop(index)
+
+                    info = {
+                        "dataset_id": dataset_id,
+                        "indices": item_buffer,
+                    }
+                    if pack_len_type == "total_block":
+                        info["longest"] = int(num_patch)
+                    elif pack_len_type == "max_block":
+                        info["longest"] = int(longest)
+
+                    pack_infos.append(info)
+
+            item_buffer = [shfl_i]
+            length_buffer = [num_tokens[shfl_i]]
+            longest = num_tokens[shfl_i]
+            num_patch = (round(num_tokens[shfl_i] / flash_attn_block_size)) ** 2
+
+    if len(item_buffer) > 0:
+        info = {
+            "dataset_id": dataset_id,
+            "indices": item_buffer,
+        }
+        if pack_len_type == "total_block":
+            info["longest"] = int(num_patch)
+        elif pack_len_type == "max_block":
+            info["longest"] = int(longest)
+        pack_infos.append(info)
+    return pack_infos
+
+
+class ExpandSoftPackDataset(SoftPackDataset):
+    def __init__(
+            self, *args, pack_len_type="total_block", flash_attn_block_size=128, pack_extra_buffer_size=1000, **kwargs
+    ):
+        self.pack_len_type = pack_len_type
+        assert self.pack_len_type in ["total_block", "max_block"], f"Invalid pack_len_type: {self.pack_len_type}"
+        self.flash_attn_block_size = flash_attn_block_size
+        self.pack_extra_buffer_size = pack_extra_buffer_size
+        self.pack_workers = int(os.environ.get("XTUNER_PACK_WORKERS", 1))
+        logger.info(f'Using {self.pack_workers} pack workers for packing datasets.')
+        super().__init__(*args, **kwargs)
+
+    def get_pack_infos(self, dataset, dataset_id, num_tokens):
+        inds = torch.randperm(len(dataset)).tolist()
+        if self.pack_workers <= 1:
+            pack_infos = get_pack_chunk_infos(inds, dataset_id, self.target, self.flash_attn_block_size,
+                                              self.pack_len_type, self.pack_extra_buffer_size, num_tokens)
+        else:
+            chunk_size = (len(inds) + self.pack_workers - 1) // self.pack_workers
+            chunks_inds = [inds[i:i + chunk_size] for i in range(0, len(inds), chunk_size)]
+
+            shm = shared_memory.SharedMemory(create=True, size=num_tokens.nbytes)
+            shm_array = np.ndarray(num_tokens.shape, dtype=num_tokens.dtype, buffer=shm.buf)
+            np.copyto(shm_array, num_tokens)
+
+            mp_context = multiprocessing.get_context("fork")
+            process_chunk_with_args = partial(
+                get_pack_chunk_infos,
+                dataset_id=dataset_id,
+                target=self.target,
+                flash_attn_block_size=self.flash_attn_block_size,
+                pack_len_type=self.pack_len_type,
+                pack_extra_buffer_size=self.pack_extra_buffer_size,
+                shm_name=shm.name,
+                shape=num_tokens.shape,
+                dtype=num_tokens.dtype
+            )
+            with ProcessPoolExecutor(max_workers=self.pack_workers, mp_context=mp_context) as executor:
+                results = list(tqdm(executor.map(process_chunk_with_args, chunks_inds)))
+
+            pack_infos = []
+            for result in results:
+                pack_infos.extend(result)
+
+            shm.close()
+            shm.unlink()
+
+        total_index = []
+        for infos in pack_infos:
+            total_index.extend(infos["indices"])
+        assert len(dataset) == len(total_index) == len(set(total_index))
+
+        pack_infos = Dataset.from_list(pack_infos)
+        return pack_infos
---
+++ xtuner/v1/datasets/sampler.py
@@ -0,0 +1,242 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+import math
+import random
+from typing import Iterator, Optional, Sized
+
+import torch
+from mmengine.dist import sync_random_seed
+from torch.distributed.device_mesh import DeviceMesh
+from torch.utils.data import ConcatDataset as TorchConcatDataset
+from torch.utils.data import Sampler
+
+
+class ParallelSampler(Sampler):
+    """The default data sampler for both distributed and non-distributed
+    environment.
+
+    It has several differences from the PyTorch ``DistributedSampler`` as
+    below:
+
+    1. This sampler supports non-distributed environment.
+
+    2. The round up behaviors are a little different.
+
+       - If ``round_up=True``, this sampler will add extra samples to make the
+         number of samples is evenly divisible by the world size. And
+         this behavior is the same as the ``DistributedSampler`` with
+         ``drop_last=False``.
+       - If ``round_up=False``, this sampler won't remove or add any samples
+         while the ``DistributedSampler`` with ``drop_last=True`` will remove
+         tail samples.
+
+    Args:
+        dataset (Sized): The dataset.
+        shuffle (bool): Whether shuffle the dataset or not. Defaults to True.
+        seed (int, optional): Random seed used to shuffle the sampler if
+            :attr:`shuffle=True`. This number should be identical across all
+            processes in the distributed group. Defaults to None.
+        round_up (bool): Whether to add extra samples to make the number of
+            samples evenly divisible by the world size. Defaults to True.
+    """
+
+    def __init__(
+            self,
+            dataset: Sized,
+            dp_mesh: DeviceMesh,
+            global_batch_size: int,
+            shuffle: bool = True,
+            seed: Optional[int] = None,
+            round_up: bool = True,
+    ) -> None:
+        super().__init__()
+        rank = dp_mesh.get_local_rank()
+        world_size = dp_mesh.size()
+
+        assert global_batch_size % world_size == 0
+        self.global_batch_size = global_batch_size
+        self.rank = rank
+        self.world_size = world_size
+
+        self.dataset = dataset
+        self.shuffle = shuffle
+        if seed is None:
+            seed = sync_random_seed()
+        self.seed = seed
+        self.epoch = 0
+        self.step = 0
+        self.round_up = round_up
+
+        if self.round_up:
+            self.num_samples = (
+                    math.ceil(len(self.dataset) / global_batch_size)
+                    * global_batch_size
+                    // world_size
+            )
+            self.total_size = self.num_samples * self.world_size
+        else:
+            self.num_samples = math.ceil((len(self.dataset) - rank) / world_size)
+            self.total_size = len(self.dataset)
+
+    def __iter__(self) -> Iterator[int]:
+        """Iterate the indices."""
+        # deterministically shuffle based on epoch and seed
+        if self.shuffle:
+            g = torch.Generator()
+            g.manual_seed(self.seed + self.epoch)
+            indices = torch.randperm(len(self.dataset), generator=g).tolist()
+        else:
+            indices = torch.arange(len(self.dataset)).tolist()
+
+        # add extra samples to make it evenly divisible
+        if self.round_up:
+            indices = (indices * int(self.total_size / len(indices) + 1))[
+                      : self.total_size
+                      ]
+
+        # subsample
+        indices = indices[self.rank: self.total_size: self.world_size]
+
+        return iter(indices[self.step:])
+
+    def __len__(self) -> int:
+        """The number of samples in this rank."""
+        return self.num_samples - self.step
+
+    def set_epoch(self, epoch: int, step=0) -> None:
+        """Sets the epoch for this sampler.
+
+        When :attr:`shuffle=True`, this ensures all replicas use a different
+        random ordering for each epoch. Otherwise, the next iteration of this
+        sampler will yield the same ordering.
+
+        Args:
+            epoch (int): Epoch number.
+        """
+        self.epoch = epoch
+        self.step = step
+
+
+def get_length_grouped_indices(max_lengths, group_batch_size, dp_size, seed=None):
+    if seed is not None:
+        torch.manual_seed(seed)
+        random.seed(seed)
+
+    assert all(leng != 0 for leng in max_lengths), "Should not have zero length."
+    indices = torch.randperm(len(max_lengths))
+    megabatches = [
+        indices[i: i + group_batch_size].tolist()
+        for i in range(0, len(max_lengths), group_batch_size)
+    ]
+    output = []
+    for megabatch in megabatches:
+        megabatch = sorted(megabatch, key=lambda i: max_lengths[i], reverse=True)
+        grouped_megabatch = [
+            megabatch[i: i + dp_size] for i in range(0, len(megabatch), dp_size)
+        ]
+        random.shuffle(grouped_megabatch)
+        for group in grouped_megabatch:
+            output.extend(group)
+
+    return output
+
+
+class LengthGroupedSampler(Sampler):
+    def __init__(
+            self,
+            dataset: Sized,
+            dp_mesh: DeviceMesh,
+            global_batch_size: int,
+            length_attr: str = "longest",
+            mega_batch_mult: Optional[int] = None,
+            seed: Optional[int] = None,
+            round_up: bool = True,
+    ) -> None:
+        super().__init__()
+        rank = dp_mesh.get_local_rank()
+        world_size = dp_mesh.size()
+        self.rank = rank
+        self.world_size = world_size
+        assert global_batch_size % world_size == 0
+
+        self.dataset = dataset
+        if seed is None:
+            seed = sync_random_seed()
+        self.seed = seed
+        self.epoch = 0
+        self.step = 0
+        self.round_up = round_up
+
+        if self.round_up:
+            self.num_samples = (
+                    math.ceil(len(self.dataset) / global_batch_size)
+                    * global_batch_size
+                    // world_size
+            )
+            self.total_size = self.num_samples * self.world_size
+        else:
+            self.num_samples = math.ceil((len(self.dataset) - rank) / world_size)
+            self.total_size = len(self.dataset)
+
+        if mega_batch_mult is None:
+            # Default for mega_batch_mult: 50 or the number to get 4
+            # megabatches, whichever is smaller.
+            mega_batch_mult = min(len(self.dataset) // (global_batch_size * 4), 50)
+            # Just in case, for tiny datasets
+            if mega_batch_mult == 0:
+                mega_batch_mult = 1
+        self.group_batch_size = mega_batch_mult * global_batch_size
+
+        if isinstance(self.dataset, TorchConcatDataset):
+            max_lengths = []
+            for sub_dataset in self.dataset.datasets:
+                if hasattr(sub_dataset, length_attr):
+                    max_lengths.extend(getattr(sub_dataset, length_attr))
+                else:
+                    raise ValueError
+            self.max_lengths = max_lengths
+        else:
+            if hasattr(self.dataset, length_attr):
+                self.max_lengths = getattr(self.dataset, length_attr)
+        assert isinstance(self.max_lengths, (list, tuple))
+
+        self.global_batch_size = global_batch_size
+
+    def __iter__(self) -> Iterator[int]:
+        """Iterate the indices."""
+        generator = torch.Generator()
+        generator.manual_seed(self.seed + self.epoch)
+        seed = self.seed + self.epoch
+        indices = get_length_grouped_indices(
+            max_lengths=self.max_lengths,
+            group_batch_size=self.group_batch_size,
+            dp_size=self.world_size,
+            seed=seed,
+        )
+        assert len(set(indices)) == len(indices)
+        # add extra samples to make it evenly divisible
+        if self.round_up:
+            indices = (indices * int(self.total_size / len(indices) + 1))[
+                      : self.total_size
+                      ]
+        # subsample
+        assert len(indices) == self.total_size
+        indices = indices[self.rank: self.total_size: self.world_size]
+        assert len(indices) == self.num_samples
+        return iter(indices[self.step:])
+
+    def __len__(self) -> int:
+        """The number of samples in this rank."""
+        return self.num_samples - self.step
+
+    def set_epoch(self, epoch: int, step=0) -> None:
+        """Sets the epoch for this sampler.
+
+        When :attr:`shuffle=True`, this ensures all replicas use a different
+        random ordering for each epoch. Otherwise, the next iteration of this
+        sampler will yield the same ordering.
+
+        Args:
+            epoch (int): Epoch number.
+        """
+        self.epoch = epoch
+        self.step = step
