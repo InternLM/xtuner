3c87d6c310e2f933a7726447087be545b9a8941d
huanghaian <huanghaian@sensetime.com>
2025-07-11 12:19:10 +0800

    add packing and sampler

--- xtuner/v1/datasets/__init__.py
+++ xtuner/v1/datasets/__init__.py
@@ -1,2 +1,18 @@
 from .jsonl import JsonlDataset
 from .utils import CachableTokenizeFunction, CacheObj, calculate_file_sha256, calculate_xxhash, tokenizer_hash
+from .packing import SoftPackDataset, ExpandSoftPackDataset
+from .sampler import ParallelSampler, LengthGroupedSampler
+
+__all__ = [
+    "JsonlDataset",
+    "CachableTokenizeFunction",
+    "CacheObj",
+    "calculate_file_sha256",
+    "calculate_xxhash",
+    "tokenizer_hash",
+    "SoftPackDataset",
+    "ExpandSoftPackDataset",
+    "ParallelSampler",
+    "LengthGroupedSampler",
+]
+
