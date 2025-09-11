7a03d7e19d2ca3ee061b51d22466e372d2d0ac62
YanhuiDua <dyh10280@163.com>
2025-09-11 13:13:22 +0000

    [bugFix] fix evaluator input

--- ci/scripts/test_grpo_trainer_bash.sh
+++ ci/scripts/test_grpo_trainer_bash.sh
@@ -1,15 +1,13 @@
 set -ex
 
-# export ROLLOUT_MODEL_PATH="/cpfs01/shared/llm_ddd/opencompass/models/modelscope_hub/QwQ/Qwen3-30B-A3B-250425"
-export ROLLOUT_MODEL_PATH="/cpfs01/shared/llm_razor/huanghaian/new_model/Qwen3-8B"
-export ROLLOUT_DATA_PATH="/cpfs01/shared/llm_razor/huanghaian/code/refactor_xtuner/gsm8k/train.jsonl"
-export ROLLOUT_TEST_DATA_PATH="/cpfs01/shared/llm_razor/huanghaian/code/refactor_xtuner/gsm8k/test.jsonl"
-export XTUNER_USE_LMDEPLOY=1 
-export PYTHONPATH='/cpfs01/shared/llm_razor/caoweihan/projects/lmdeploy':'/cpfs01/shared/llm_ddd/caoweihan/projects/Liger-Kernel/src/':'.':$PYTHONPATH 
-export UVICORN_LOG_LEVEL="CRITICAl"
+MODEL_PATH=$1
+TRAIN_DATA_PATH=$2
+TEST_DATA_PATH=$3
+
+export XTUNER_USE_LMDEPLOY=1
+export UVICORN_LOG_LEVEL="CRITICAL"
 export PYTORCH_CUDA_ALLOC_CONF='expandable_segments:True'
 
-# OUTPUT_DIR='work_dirs/dense_8b_gsm8k_grpo_fix_shuaibin'
 OUTPUT_DIR='work_dirs/debug'
 if [ ! -d "$OUTPUT_DIR" ]; then
   mkdir -p "$OUTPUT_DIR"
@@ -18,11 +16,14 @@ fi
 python ci/scripts/test_grpo_trainer.py \
     --total-epochs 15 \
     --work-dir "$OUTPUT_DIR" \
+    --model-path "$MODEL_PATH" \
+    --train-data-path "$TRAIN_DATA_PATH" \
+    --test-data-path "$TEST_DATA_PATH" \
     --num-workers 8 \
     --gpus-per-node 8 \
     --rollout-global-batch-size 1024 \
     --train-optimizer-steps 4 \
-    --max-concurrent 64 \
+    --max-concurrent 512 \
     --prompt-repeat-k 5 \
     --pack-max-length 32768 \
     --max-prompt-length 512 \
