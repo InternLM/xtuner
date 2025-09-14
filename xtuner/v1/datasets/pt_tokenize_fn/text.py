62ff582f09bfc90d4eb94a6275c94c5582e8b70a
pppppM <gjf_mail@126.com>
2025-09-14 17:07:14 +0000

    add pt example

---
+++ xtuner/v1/datasets/pt_tokenize_fn/text.py
@@ -0,0 +1,83 @@
+import copy
+import hashlib
+import inspect
+from typing import Annotated, Union
+
+from cyclopts import Parameter
+from pydantic import BaseModel
+
+from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
+from xtuner.v1.datasets.data_item import CacheItem, DataItem
+
+from ..utils import CachableTokenizeFunction, tokenizer_xxhash
+
+
+class PretrainTokenizeFunction(CachableTokenizeFunction[DataItem]):
+    def __init__(
+        self,
+        tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
+        add_eos_token: bool = True,
+        add_special_tokens: bool = False,
+        tokenizer_hash: str | None = None,
+        hash: str | None = None,
+    ):
+        self.tokenizer = tokenizer
+        self.add_eos_token = add_eos_token
+        self.add_special_tokens = add_special_tokens
+        self._tokenizer_hash = tokenizer_hash
+        self._hash = hash
+
+    def __call__(self, item: dict, **kwargs) -> DataItem | CacheItem:
+        text = item["content"]
+
+        # if self.add_bos_token:
+        #     text = self.tokenizer.bos_token + text
+        if self.add_eos_token:
+            text = text + self.tokenizer.eos_token
+
+        input_ids = self.tokenizer.encode(text, add_special_tokens=self.add_special_tokens)
+        num_tokens = len(input_ids)
+        labels = copy.deepcopy(input_ids)
+        labels[0] = -100
+        return {"input_ids": input_ids, "labels": labels, "num_tokens": num_tokens}
+
+    def hash(self) -> str:
+        if self._hash is None:
+            # truncate to 16 characters prevent too long cache directory
+            if self._tokenizer_hash is None:
+                _tokenizer_hash = tokenizer_xxhash(self.tokenizer)[:16]
+            else:
+                _tokenizer_hash = self._tokenizer_hash
+
+            _source_hash = (
+                hashlib.sha256(inspect.getsource(self.__class__.__call__).encode()).hexdigest()[:16]
+                + hashlib.sha256(inspect.getsource(self.__class__.__init__).encode()).hexdigest()[:16]
+            )
+
+            self._hash = f"{_tokenizer_hash}_{_source_hash}_{self.add_special_tokens}_{self.add_eos_token}"
+        else:
+            assert isinstance(self._hash, str), (
+                "hash is not a valid string, it means `FtdpTokenizeFunction._hash` is modified by user."
+            )
+
+        return self._hash
+
+
+class PretrainTokenizeFunctionConfig(BaseModel):
+    add_eos_token: Annotated[bool, Parameter(group="tokenize_fn")] = True
+    add_special_tokens: Annotated[bool, Parameter(group="tokenize_fn")] = False
+    hash: Annotated[str | None, Parameter(group="tokenize_fn")] = None
+
+    def build(
+        self,
+        tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
+        tokenizer_hash: str | None = None,
+        anno_name: str | None = None,
+        **kwargs,
+    ) -> PretrainTokenizeFunction:
+        return PretrainTokenizeFunction(
+            tokenizer=tokenizer,
+            add_eos_token=self.add_eos_token,
+            add_special_tokens=self.add_special_tokens,
+            tokenizer_hash=tokenizer_hash,
+        )
