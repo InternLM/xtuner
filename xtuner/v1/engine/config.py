a4f60a21e702f780226a6aaaf8d86d5d7960c350
huanghaian <huanghaian@sensetime.com>
2025-09-15 20:00:49 +0800

    fix import

--- xtuner/v1/engine/config.py
+++ xtuner/v1/engine/config.py
@@ -4,7 +4,6 @@ from pydantic import BaseModel as PydanticBaseModel
 
 from xtuner.v1.config import FSDPConfig, OptimConfig
 from xtuner.v1.model.base import BaseModel
-from xtuner.v1.model.compose.intern_s1 import InternS1BaseConfig
 
 
 @runtime_checkable
@@ -22,10 +21,9 @@ class EngineConfig(PydanticBaseModel):
 
     def build(self):
         from xtuner.v1.engine.train_engine import TrainEngine
-        from xtuner.v1.engine.vision_compose_train_engine import InternS1TrainEngine
+        from xtuner.v1.engine.vision_compose_train_engine import VisionComposeConfigProtocol, VisionComposeTrainEngine
 
-        # TODO: (hha) Remove this hardcode. Use Composable Engine rather than InternS1BaseEngine
-        if isinstance(self.model_cfg, InternS1BaseConfig):
-            return InternS1TrainEngine(model_cfg=self.model_cfg, optim_cfg=self.optim_cfg, fsdp_cfg=self.fsdp_cfg)
+        if isinstance(self.model_cfg, VisionComposeConfigProtocol):
+            return VisionComposeTrainEngine(model_cfg=self.model_cfg, optim_cfg=self.optim_cfg, fsdp_cfg=self.fsdp_cfg)
         else:
             return TrainEngine(model_cfg=self.model_cfg, optim_cfg=self.optim_cfg, fsdp_cfg=self.fsdp_cfg)
