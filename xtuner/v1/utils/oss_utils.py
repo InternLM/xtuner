138b15210af4b264e212baabaee1c2196d6c5b8f
huanghaian <huanghaian@sensetime.com>
2025-10-11 10:13:11 +0800

    fix

--- xtuner/v1/utils/oss_utils.py
+++ xtuner/v1/utils/oss_utils.py
@@ -22,7 +22,7 @@ def get_oss_backend(backend: Literal["petrel"] = "petrel", **kwargs):
 
     if backend not in oss_backend_instances:
         if oss_backends[backend] is None:
-            raise ImportError(f"{backend} backend requires petrel-client to be installed.")
+            raise ImportError(f"{backend} backend import failed, please install it first.")
         logger.info(f"Initializing OSS backend: {backend}, kwargs: {kwargs}")
         oss_backend_instances[backend] = oss_backends[backend](**kwargs)
     return oss_backend_instances[backend]
