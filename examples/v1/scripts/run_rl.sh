b054d16e99433c9232e01dfa083be76239fa4b97
YanhuiDua <dyh10280@163.com>
2025-11-12 17:23:36 +0800

    fix

--- examples/v1/scripts/run_rl.sh
+++ examples/v1/scripts/run_rl.sh
@@ -11,23 +11,25 @@ MODEL_PATH=$3
 DATA_PATH=$4
 EVAL_DATA_PATH=${5:-""}
 
-# 1. 环境配置
-# 系统环境变量
 export PYTHONPATH=$(pwd):$PYTHONPATH
 
 # ray 环境变量
+export MASTER_PORT=6000
+export WORLD_SIZE=$NODE_COUNT
+export RANK=$NODE_RANK
 export RAY_MASTER_ADDR=${MASTER_ADDR:-"127.0.0.1"}
 export RAY_RANK=${RANK:-0} # 0 代表主节点, >0 代表工作节点
 export RAY_HEAD_PORT=${RAY_HEAD_PORT:-"6379"}
 export RAY_CLIENT_PORT=${RAY_CLIENT_PORT:-"10001"}
 export RAY_DASHBOARD_PORT=${RAY_DASHBOARD_PORT:-"8265"}
+# TODO: 提供非环境变量方式配置 ray_max_concurrency
+export RAY_MAX_CONCURRENCY=${RAY_MAX_CONCURRENCY:-1024} # dataflow_max_concurrency * prompt_repeat_k
 
 # xtuner 环境变量
 export MODEL_PATH=$MODEL_PATH
 export DATA_PATH=$DATA_PATH
 export EVAL_DATA_PATH=$EVAL_DATA_PATH
 export XTUNER_USE_FA3=${XTUNER_USE_FA3:-1}
-export XTUNER_MAX_CONCURRENCY=${XTUNER_MAX_CONCURRENCY:-512}
 export XTUNER_LOG_LEVEL=${XTUNER_LOG_LEVEL:-"INFO"}
 
 infer_backend_lower=$(echo "$INFER_BACKEND" | tr '[:upper:]' '[:lower:]')
@@ -70,8 +72,7 @@ if [ "$RAY_RANK" -eq 0 ]; then
     --num-cpus=$total_cpus
 else
   while true; do
-    curl --connect-timeout 2 "http://${RAY_MASTER_ADDR}:${RAY_DASHBOARD_PORT}" >/dev/null 2>&1
-    if [ $? -eq 0 ]; then
+    if curl --connect-timeout 2 "http://${RAY_MASTER_ADDR}:${RAY_DASHBOARD_PORT}" >/dev/null 2>&1; then
       echo "Successfully connected to Ray master at ${RAY_MASTER_ADDR}:${RAY_DASHBOARD_PORT}"
       break
     else
