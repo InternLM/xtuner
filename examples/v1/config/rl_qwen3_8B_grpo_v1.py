f6a0242b3fff166eeda19bac82b80639b2fb49e8
YanhuiDua <dyh10280@163.com>
2025-11-12 14:56:09 +0800

    fix

--- examples/v1/config/rl_qwen3_8B_grpo_v1.py
+++ examples/v1/config/rl_qwen3_8B_grpo_v1.py
@@ -7,7 +7,7 @@ from xtuner.v1.model.dense.qwen2 import Qwen2Dense7BConfig
 from xtuner.v1.train.rl_trainer import RLTrainerConfig
 
 from xtuner.v1.utils.rl_common_config import (
-    get_dapo_judger_config,
+    get_gsm8k_judger_config,
     get_dataflow_config,
     get_evaluator_config,
     get_replay_buffer_config,
@@ -23,26 +23,26 @@ params = {
     "data_path": os.environ.get("DATA_PATH", ""),
     "eval_data_path": os.environ.get("EVAL_DATA_PATH", ""),
     # training settings
-    "total_epochs": 1,
-    "train_optimizer_steps": 16,
-    "hf_interval": 50,
+    "total_epochs": 15,
+    "train_optimizer_steps": 4,
+    "hf_interval": 15,
     # model settings
-    "max_prompt_length": 2048,
-    "max_response_length": 8192,
+    "max_prompt_length": 512,
+    "max_response_length": 1024,
     # rollout settings
     "tensor_parallel_size": 2,
     "expert_parallel_size": 1,
     "rollout_max_batch_size_per_instance": 1024,
     # dataflow settings
-    "global_batch_size": 512,
-    "prompt_repeat_k": 16,
+    "global_batch_size": 1024,
+    "prompt_repeat_k": 5,
     "enable_partial_rollout": 0,
     "partial_rollout_step": 0,
     "max_concurrent": 512,
     # evaluate settings
     "enable_evaluate": os.environ.get("EVAL_DATA_PATH") != "",
     "enable_initial_evaluate": True,
-    "evaluate_step": 5,
+    "evaluate_step": 10,
     # resource setting
     "num_workers": 8,
 }
@@ -52,22 +52,18 @@ params["context_length"] = params["pack_max_length"]
 
 # 创建核心组件
 tokenizer = AutoTokenizer.from_pretrained(params["model_path"], trust_remote_code=True)
-training_sample_params = SampleParams(
-    max_tokens=params["max_response_length"], top_k=0, top_p=1.0, temperature=1.0, min_tokens=0
-)
+training_sample_params = SampleParams(max_tokens=params["max_response_length"])
 evaluation_sample_params = deepcopy(training_sample_params)
-evaluation_sample_params.top_p = 0.7
-
-
-def dapo_compute_metric(samples):
-    return {"accuracy": sum(s.env.judger.reward["acc"] > 0 for s in samples) / len(samples)}
+evaluation_sample_params.top_p = 1.0
+evaluation_sample_params.temperature = 0.0
+evaluation_sample_params.top_k = 1.0
 
 resource_cfg = get_resources_config(**params)
 rollout_cfg = get_rollout_config(**params)
-dapo_judger_cfg = get_dapo_judger_config(tokenizer=tokenizer, **params)
+gsm8k_judger_cfg = get_gsm8k_judger_config()
 dataflow_cfg = get_dataflow_config(sample_params=training_sample_params, **params)
 replay_buffer_cfg = get_replay_buffer_config(tokenizer=tokenizer, **params)
-evaluator_cfg = get_evaluator_config(tokenizer=tokenizer,compute_metric_func=dapo_compute_metric,sample_params=evaluation_sample_params,**params)
+evaluator_cfg = get_evaluator_config(tokenizer=tokenizer,sample_params=evaluation_sample_params,**params)
 train_worker_cfg = get_train_worker_config(**params)
 
 # 组装RL trainer
@@ -80,7 +76,7 @@ trainer = RLTrainerConfig(
     resources=resource_cfg,
     rollout_config=rollout_cfg,
     dataflow_config=dataflow_cfg,
-    judger_config=dapo_judger_cfg,
+    judger_config=gsm8k_judger_cfg,
     replay_buffer_config=replay_buffer_cfg,
     evaluator_config=evaluator_cfg,
     train_worker_config=train_worker_cfg,
