d3aec12daf278ae73e768be4844f29ff45a7ab2a
YanhuiDua <dyh10280@163.com>
2025-11-13 21:50:36 +0800

    fix

--- xtuner/v1/ray/dataflow/flow.py
+++ xtuner/v1/ray/dataflow/flow.py
@@ -115,7 +115,7 @@ class DataFlow:
         self.finished_samples_count = 0
         self.unfinished_samples_count = 0
         self.failed_samples_count = 0
-        self.input_error_sample_count = 0
+        self.skipped_sample_count = 0
         self.logger = get_logger(log_dir=self.config.worker_log_dir, tag="DataFlow")
         self.target_batch_size = self.config.global_batch_size
         self.logger.info(f"DataFlowConfig:\n{self.config.model_dump_json(indent=2)}")
@@ -198,7 +198,7 @@ class DataFlow:
                 f"Bad request for {group_data_items[0].uid.action_id} response. Skipping this request."
             )
             self.logger.debug(f"Worker task skipped successfully for {group_data_items[0].uid.action_id}.")
-            self.input_error_sample_count += 1
+            self.skipped_sample_count += 1
             return
 
         # step 3: filter
@@ -237,7 +237,7 @@ class DataFlow:
             while (
                 self.finished_samples_count < self.target_batch_size
                 and self.failed_samples_count < self.target_batch_size
-                and self.input_error_sample_count < self.target_batch_size
+                and self.skipped_sample_count < self.target_batch_size
             ):
                 if self.finished_samples_count >= next_update_threshold:
                     pbar.n = self.finished_samples_count
@@ -279,10 +279,7 @@ class DataFlow:
 
         if self.finished_samples_count >= self.target_batch_size:
             self.logger.info("Target batch size reached. Pausing env controller.")
-        if (
-            self.failed_samples_count >= self.target_batch_size
-            or self.input_error_sample_count >= self.target_batch_size
-        ):
+        if self.failed_samples_count >= self.target_batch_size or self.skipped_sample_count >= self.target_batch_size:
             self.logger.info("Max failed samples reached. Pausing env controller.")
 
         # NOTE: Directly send pause requests to rollout workers because calling `rollout_controller.pause()`
@@ -356,7 +353,7 @@ class DataFlow:
         self.finished_samples_count = 0
         self.unfinished_samples_count = 0
         self.failed_samples_count = 0
-        self.input_error_sample_count = 0
+        self.skipped_sample_count = 0
         self.target_batch_size = num if num and num > 0 else self.config.global_batch_size
         self.logger.info(f"Start generate dataflow and target batch size set to {self.target_batch_size}.")
         self.sample_params = sample_params if sample_params else self.config.sample_params
