[Fix] dist.broadcast/dist.broadcast_object_list should use global rank instead of local rank in pg

xtuner/v1/rl/base/worker.py
@@ -628,21 +628,20 @@ class TrainingWorker(SingleAcceleratorWorker):
             state_dict = {name: param.detach() for name, param in zip(name_list, fused_param_list)}
             if model.fsdp_config.ep_size > 1:
                 ep_mesh: DeviceMesh = model.ep_mesh
-                ep_size = ep_mesh.size()
                 ep_group = ep_mesh.get_group()
                 ep_rank = dist.get_rank(group=ep_group)
-                for src_rank in range(ep_size):
+                for src_global_rank in dist.get_process_group_ranks(ep_group):
                     broadcast_state_dict = dict()
                     for key, tensor in state_dict.items():
-                        obj_to_broadcast = [key, tensor.to("meta")] if ep_rank == src_rank else [None, None]
-                        dist.broadcast_object_list(obj_to_broadcast, src=src_rank, group=ep_group)
+                        obj_to_broadcast = [key, tensor.to("meta")] if ep_rank == src_global_rank else [None, None]
+                        dist.broadcast_object_list(obj_to_broadcast, src=src_global_rank, group=ep_group)
                         real_key, meta_tensor = obj_to_broadcast
                         buffer = (
                             state_dict[real_key]
-                            if ep_rank == src_rank
+                            if ep_rank == src_global_rank
                             else torch.empty_like(meta_tensor, device=DEVICE)
                         )
-                        dist.broadcast(buffer, src=src_rank, group=ep_group)
+                        dist.broadcast(buffer, src=src_global_rank, group=ep_group)
                         broadcast_state_dict[real_key] = buffer
                     self.request_update_params(broadcast_state_dict, finished=False)
                     del broadcast_state_dict, buffer