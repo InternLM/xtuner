aedff578fddff7f72e785f4420fe272ac0f9ba73
kkscilife <1658148753@qq.com>
2025-12-02 11:20:44 +0800

    add utils script

---
+++ autotest/utils/pre_action.py
@@ -0,0 +1,14 @@
+import subprocess
+
+def run_cmd(command):
+    try:
+        result = subprocess.run(
+            command,
+            shell=True,
+            capture_output=True,
+            text=True        
+            )       
+        if result.returncode != 0:
+            print(f"run command error:{result.stderr}")
+    except Exception as e:
+        print(f"Unknown error: {e}")
