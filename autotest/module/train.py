58dc41b8a88ac25894ca2edf3d7b7b9d5d257c6b
zhulin1 <zhulinJulia24@163.com>
2025-12-03 10:29:04 +0800

    fix unimport

--- autotest/module/train.py
+++ autotest/module/train.py
@@ -1,6 +1,7 @@
 import os
 
 from utils.check_metric import check_result
+from utils.run_cmd import run_cmd
 
 
 class Train:
@@ -24,12 +25,13 @@ class Train:
             )
 
             command = (
-                "cd {current_dir}; pwd; torchrun --nproc-per-node 8 --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} --nnodes=${WORLD_SIZE} --node_rank=${RANK} "
+                f"cd {current_dir}; pwd; "
+                + "torchrun --nproc-per-node 8 --master_addr=${MASTER_ADDR} --master_port=${MASTER_PORT} --nnodes=${WORLD_SIZE} --node_rank=${RANK} "
                 + f"xtuner/v1/train/cli/{train_type}.py"
             )
             if config_path:
                 output_path = model_config = config.get("parameters", {}).get("output_path", ".")
-                if output_path == '.':
+                if output_path == ".":
                     command += f" --config {config_path}; mkdir -p {work_dir}; mv {output_path}/.xtuner {work_dir}; mv {output_path}/202* {work_dir}"
                 else:
                     command += f" --config {config_path}"
@@ -55,7 +57,7 @@ class Train:
         cur_path = os.path.join(get_latest_subdir(work_dir), "logs/exp_tracking/rank0/tracker.jsonl")
         check_metrics = config.get("assert_info", {}).get("check_metrics", {})
         return check_result(base_path, cur_path, check_metrics)
-    
+
     def pre_action(config=None):
         action_info = config.get("pre_action", None)
         if action_info:
--- autotest/utils/run_cmd.py
+++ autotest/utils/run_cmd.py
@@ -1,13 +1,9 @@
 import subprocess
 
+
 def run_cmd(command):
     try:
-        result = subprocess.run(
-            command,
-            shell=True,
-            capture_output=True,
-            text=True        
-            )       
+        result = subprocess.run(command, shell=True, capture_output=True, text=True)
         if result.returncode != 0:
             print(f"run command error:{result.stderr}")
     except Exception as e:
