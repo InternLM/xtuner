e261c7d88b8a58be2f34d2f7eafb677a0c79bf7c
Haian Huang(深度眸) <1286304229@qq.com>
2025-12-03 18:04:47 +0800

    Fix RL Mem Leak and add track (#1314)
    * update
    
    * add py
    
    * add py
    
    * add py
    
    * add py
    
    * fix
    
    * fix
    
    * fix config

--- xtuner/v1/ray/dataflow/replay_buffer.py
+++ xtuner/v1/ray/dataflow/replay_buffer.py
@@ -90,6 +90,8 @@ def mapping_replaymeta_to_dataitem(replay_meta: ReplayMeta) -> List[RLDataFlowIt
         replay_meta.observation_ids, replay_meta.observation_refs, replay_meta.observation_versions
     ):
         env_data = ray.get(obs_ref)
+        ray._private.internal_api.free(obs_ref)
+
         item = RLDataFlowItem(
             uid=RLUIDItem(env=env_str, root_id=root_id, action_id=action_id, observation_id=obs_id, version=version),
             data=data_ref,
@@ -361,10 +363,11 @@ class ReplayBufferStorage:
             target_finished_list = self._returned[:global_batch_size]
             remain_finished_list = self._returned[global_batch_size:]
             for action_id in target_finished_list:
-                replay_meta = self._actions[action_id]
+                replay_meta = self._actions.pop(action_id)
                 # todo: add an unified state management
                 replay_meta.state = "history"
-                group_samples = mapping_replaymeta_to_dataitem(self._actions[action_id])
+                group_samples = mapping_replaymeta_to_dataitem(replay_meta)
+                del replay_meta
                 multimodal_train_info = None
                 # TODO: 是否需要额外返回不重复的 multimodal_train_infos？
                 for data_item in group_samples:
