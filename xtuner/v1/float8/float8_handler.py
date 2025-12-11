d0d3365c2810de7ba37d8ef9368aa7cf35efac81
HIT-cwh <2892770585@qq.com>
2025-12-11 20:54:11 +0800

    support bf16 gemm fp8 gmm or fp8 gemm bf16 gmm

--- xtuner/v1/float8/float8_handler.py
+++ xtuner/v1/float8/float8_handler.py
@@ -36,8 +36,8 @@ def default_grouped_linear_filter_fn(mod: nn.Module, fqn: str):
 
 # handler 要跟 Engine 一一对应？
 class Float8Handler:
-    scaling_granularity_gemm: ScalingGranularity
-    scaling_granularity_grouped_gemm: ScalingGranularity
+    scaling_granularity_gemm: ScalingGranularity | None
+    scaling_granularity_grouped_gemm: ScalingGranularity | None
     fsdp_mesh: Optional[DeviceMesh] = None
     tilewise_reduce_mesh_devided_64: Optional[DeviceMesh] = None
     tilewise_reduce_mesh_mapping: Dict[Tuple[int, int], DeviceMesh] = {}
@@ -61,12 +61,14 @@ class Float8Handler:
             )
             return
 
-        assert scaling_granularity_gemm in (ScalingGranularity.TILEWISE, ScalingGranularity.TENSORWISE), (
-            "scaling_granularity_gemm must be TILEWISE or TENSORWISE."
+        assert scaling_granularity_gemm in (ScalingGranularity.TILEWISE, ScalingGranularity.TENSORWISE, None), (
+            "scaling_granularity_gemm must be TILEWISE, TENSORWISE or None."
         )
-        assert scaling_granularity_grouped_gemm in (ScalingGranularity.TILEWISE, ScalingGranularity.TENSORWISE), (
-            "scaling_granularity_grouped_gemm must be TILEWISE or TENSORWISE."
-        )
+        assert scaling_granularity_grouped_gemm in (
+            ScalingGranularity.TILEWISE,
+            ScalingGranularity.TENSORWISE,
+            None,
+        ), "scaling_granularity_grouped_gemm must be TILEWISE or TENSORWISE."
 
         self.scaling_granularity_gemm = scaling_granularity_gemm
         self.scaling_granularity_grouped_gemm = scaling_granularity_grouped_gemm
