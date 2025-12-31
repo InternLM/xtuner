ae00df1e2a2a8a68d796066d70fd1d623a2c2ce2
huanghaian <huanghaian@sensetime.com>
2025-12-31 17:16:40 +0800

    add vl ci

--- tests/datasets/test_qwen3_vl_tokenize_fn.py
+++ tests/datasets/test_qwen3_vl_tokenize_fn.py
@@ -18,6 +18,73 @@ class TestMLLMTokenizeFn(TestCase):
             self.tokenizer)
         self.processor = AutoProcessor.from_pretrained(QWEN3_VL_PATH)
 
+    def test_qwen3vl_tool_template(self):
+        tokenize_fn = Qwen3VLTokenizeFnConfig(processor_path=QWEN3_VL_PATH).build(self.tokenizer)
+
+        messages = {
+            "messages": [
+                {"role": "system", "content": "这是系统消息"},
+                {"role": "user", "content": "Hey, what's the temperature in Paris right now?"},
+                {"role": "assistant", "content": "我需要先调用一些工具才能知道",
+                 "tool_calls": [
+                     {
+                         "id": "call_123",
+                         "type": "function",
+                         "function": {
+                             "name": "get_weather",
+                             "arguments": "{\"location\": \"Boston\"}"
+                         }
+                     },
+                     {
+                         "id": "call_456",
+                         "type": "function",
+                         "function": {
+                             "name": "get_weather",
+                             "arguments": "{\"location\": \"beijing \"}"
+                         }
+                     }
+                 ],
+                 "reasoning_content": ""},
+                {"role": "tool", "content": "22"},
+                {"role": "assistant", "content": "你问的特别好"}
+            ],
+            "tools": [{
+                "type": "function",
+                "function": {
+                    "name": "get_current_temperature",
+                    "description": "Gets the temperature at a given location.",
+                    "parameters": {
+                        "type": "object",
+                        "properties": {
+                            "location": {
+                                "type": "string",
+                                "description": "The location to get the temperature for"
+                            }
+                        },
+                        "required": [
+                            "location"
+                        ]
+                    }
+                }
+            },
+                {"type": "function", "function": {"name": "get_current_wind_speed",
+                                                  "description": "Get the current wind speed in km/h at a given "
+                                                                 "location.",
+                                                  "parameters": {"type": "object", "properties": {
+                                                      "location": {"type": "string",
+                                                                   "description": "The location to get the wind speed for, in the format \"City, Country\""}},
+                                                                 "required": ["location"]}}}
+            ],
+        }
+        input_ids_ref = self.tokenizer.apply_chat_template(
+            messages["messages"],
+            tools=messages["tools"],
+            tokenize=True,
+            add_generation_prompt=False,
+        )
+        input_ids = tokenize_fn(messages)['input_ids']
+        self.assertEqual(input_ids, input_ids_ref)
+
     def test_qwen3_vl_sft_single_image(self):
         data_path = 'tests/resource/mllm_sft_single_image_example_data.jsonl'
         total_step = 5
