8dae89986f950a474413c1a2e47e014330021511
Haian Huang(深度眸) <1286304229@qq.com>
2025-12-31 14:53:20 +0800

    add sft training with RL (#1403)
    * add sft trainging with RL
    
    * fix lint
    
    * update
    
    * refine
    
    * fix comment
    
    * add ci

--- xtuner/v1/rl/base/worker.py
+++ xtuner/v1/rl/base/worker.py
@@ -1,29 +1,36 @@
 import json
 import math
 import os
+import time
 from itertools import chain
 from pathlib import Path
-from typing import Dict, List, TypeAlias, TypedDict, cast
+from typing import Dict, Iterable, List, TypeAlias, TypedDict, cast
 
 import ray
 import requests
 import torch
 import torch.distributed as dist
 import tqdm
+from mmengine.runner import set_random_seed
 from pydantic import BaseModel, ConfigDict
 from ray.actor import ActorClass, ActorProxy
 from torch.distributed.device_mesh import DeviceMesh, init_device_mesh
 from torch.distributed.tensor import DTensor
 from typing_extensions import NotRequired
 
+from transformers import AutoTokenizer
 from xtuner.v1.config.fsdp import FSDPConfig
 from xtuner.v1.config.optim import LRConfig, OptimConfig
 from xtuner.v1.data_proto.sequence_context import SequenceContext
+from xtuner.v1.datasets.config import DataloaderConfig
+from xtuner.v1.datasets.dataloader import Dataloader
 from xtuner.v1.engine.train_engine import LossLog, OtherLog, TrainEngine
 from xtuner.v1.engine.vision_compose_train_engine import (
     VisionComposeTrainEngine,
 )
 from xtuner.v1.float8.float8_handler import Float8Handler
+from xtuner.v1.loss import CELossConfig
+from xtuner.v1.loss.ce_loss import CELossContextInputItem
 from xtuner.v1.model.base import BaseModel as XtunerBaseModel
 from xtuner.v1.model.base import ModelItem, TransformerConfig
 from xtuner.v1.model.compose.base import BaseComposeConfig, BaseComposeModel
@@ -33,6 +40,7 @@ from xtuner.v1.ray.config import RolloutConfig
 from xtuner.v1.rl.utils import gather_logprobs
 from xtuner.v1.train.trainer import LoadCheckpointConfig
 from xtuner.v1.utils import (
+    XTUNER_DETERMINISTIC,
     ParallelConfigException,
     get_device,
     get_logger,
@@ -133,6 +141,13 @@ class WorkerConfig(BaseModel):
     ref_model_fsdp_cfg: FSDPConfig | None = None
     log_dir: str | Path | None = None
     update_weight_bucket_size_in_gb: float = 0.5  # 512MB
+    seed: None | int = None  # if None, use RLTrainer seed
+
+    # sft config
+    sft_dataloader_cfg: DataloaderConfig | None = None
+    sft_global_batch_size: int = -1
+    rollout_steps_per_sft: int = 1
+    sft_loss_cfg: CELossConfig = CELossConfig()
 
 
 class WorkerInputItem(TypedDict):
@@ -163,11 +178,14 @@ class WorkerLogItem(TypedDict):
     mismatch_metrics: NotRequired[dict[str, float]]
     rollout_is_metrics: NotRequired[dict[str, float]]
     train_metrics: List[WorkerTrainLogItem]
+    sft_train_metrics: NotRequired[dict[str, float]]
 
 
 class TrainingWorker(SingleAcceleratorWorker):
     _SAVE_OPTIMIZER_DIR = "optimizer"
     _SAVE_MODEL_DIR = "model"
+    _SAVE_SFT_DATALOADER_DIR = "sft_dataloader"
+    _SAVE_SFT_TRAIN_STATE_PATH = "sft_train_state.json"
 
     def __init__(
         self,
@@ -192,6 +210,14 @@ class TrainingWorker(SingleAcceleratorWorker):
         else:
             self.logger = get_logger()
 
+        self._set_deterministic()
+        self._set_random_seed(worker_cfg.seed)
+
+        self.data_mesh = self._init_data_mesh(sp_size=worker_cfg.sp_size)
+        self.sp_mesh = self.data_mesh["sp"]
+
+        self._init_sft(worker_cfg)
+
         if not worker_cfg.fsdp_cfg.torch_compile:
             worker_cfg.model_cfg.compile_cfg = False
         self._engine = self._build_engine(worker_cfg)
@@ -205,8 +231,6 @@ class TrainingWorker(SingleAcceleratorWorker):
                 worker_cfg.model_cfg, worker_cfg.ref_load_from, worker_cfg.ref_model_fsdp_cfg
             )
 
-        self.data_mesh = self._init_data_mesh(sp_size=worker_cfg.sp_size)
-        self.sp_mesh = self.data_mesh["sp"]
         self._optimizer_steps = worker_cfg.optimizer_steps
 
         # Used to update weight to rollout engine
@@ -215,6 +239,45 @@ class TrainingWorker(SingleAcceleratorWorker):
         self.rollout_cfg_info: dict = dict()
         self.endpoints: dict[str, str] = dict()
         self.endpoints["update_weights"] = "update_weights"
+
+    def _init_sft(self, worker_cfg):
+        self._sft_dataloader_config = worker_cfg.sft_dataloader_cfg
+        self._sft_dataloader: Dataloader | None = None
+        self._sft_dataloader_iter: Iterable | None = None
+        self._sft_loss_cfg: CELossConfig | None = None
+        self._rollout_steps_per_sft = worker_cfg.rollout_steps_per_sft
+
+        self._rollout_step = 0
+        self._sft_cur_epoch = 0
+        self._sft_total_consumed_samples = 0
+        self._sft_total_consumed_tokens = 0
+
+        if self._sft_dataloader_config is not None:
+            assert worker_cfg.sft_global_batch_size > 0, "sft_global_batch_size must be greater than 0"
+            assert worker_cfg.seed is not None, "seed must be set when sft_dataloader_config is not None"
+            tokenizer = AutoTokenizer.from_pretrained(worker_cfg.load_from, trust_remote_code=True)
+            self._sft_dataloader = self._sft_dataloader_config.build(
+                tokenizer=tokenizer,
+                dp_mesh=self.data_mesh["dp"],
+                global_batch_size=worker_cfg.sft_global_batch_size,
+                micro_batch_size=1,
+                seed=worker_cfg.seed,
+            )
+            self.logger.info(f"Sft Dataloader len: {len(self._sft_dataloader)}")
+
+            sft_loss_cfg = worker_cfg.sft_loss_cfg
+            if worker_cfg.sft_loss_cfg is None:
+                sft_loss_cfg = CELossConfig()
+            self._sft_loss_cfg = sft_loss_cfg
+
+    def _set_deterministic(self):
+        if XTUNER_DETERMINISTIC:
+            self.logger.info("Setting deterministic algorithms of TrainingWorker.")
+            os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
+            torch.use_deterministic_algorithms(True, warn_only=True)
+
+    def _set_random_seed(self, seed: None | int):
+        set_random_seed(seed)
 
     def _build_engine(self, worker_cfg: WorkerConfig) -> TrainEngine | VisionComposeTrainEngine:
         if isinstance(worker_cfg.model_cfg, BaseComposeConfig):
@@ -491,10 +554,7 @@ class TrainingWorker(SingleAcceleratorWorker):
                 all_rollout_is_metrics.append(rollout_is_metrics)
                 all_mismatch_metrics.append(mismatch_metrics)
 
-        worker_log_item: WorkerLogItem = {
-            "train_entropy": 0.0,
-            "train_metrics": [],
-        }
+        worker_log_item: WorkerLogItem = {"train_entropy": 0.0, "train_metrics": [], "sft_train_metrics": {}}
         logger_msg = f"Rollout {rollout_idx}: "
         sum_entropy = cast(torch.Tensor, sum_entropy)
         dist.all_reduce(sum_entropy, op=dist.ReduceOp.SUM)
@@ -578,10 +638,142 @@ class TrainingWorker(SingleAcceleratorWorker):
                 f"{key}={value:.4f}" if isinstance(value, float) else f"{key}={value}"
                 for key, value in log_info.items()
             )
-            log_str = f"Rollout {rollout_idx} Step {i}: " + log_str
+            log_str = f"Rank{self.rank} Rollout {rollout_idx} Step {i}: " + log_str
             self.logger.info(log_str)
 
+        self._rollout_step += 1
+        if self._sft_dataloader is not None and self._rollout_step % self._rollout_steps_per_sft == 0:
+            loss_log = self._fit_sft()
+            worker_log_item["sft_train_metrics"] = loss_log
+
         return worker_log_item
+
+    def _fit_sft(self):
+        self.logger.info(f"Train SFT after {self._rollout_step} RL steps")
+        if self._sft_dataloader_iter is None:
+            self._sft_dataloader_iter = iter(self._sft_dataloader)
+
+        time_before_get_data = time.time()
+        data_batch = self._next_sft_data_batch()
+        time_before_train_step = time.time()
+        data_time = time_before_train_step - time_before_get_data
+        DEVICE_MODULE.reset_peak_memory_stats()
+        cur_sample_num = len(data_batch)
+
+        loss_log, other_log, grad_norm = self._train_one_step_sft(data_batch)
+
+        time_after_train_step = time.time()
+        step_time = time_after_train_step - time_before_train_step
+        step_consumed_tokens = other_log["step_consumed_tokens"]
+
+        self._sft_total_consumed_samples += self._reduce_number_across_rank(cur_sample_num)
+        reduced_step_consumed_tokens = self._reduce_number_across_rank(step_consumed_tokens)
+        self._sft_total_consumed_tokens += reduced_step_consumed_tokens
+
+        self._sft_log_step(
+            loss_log=loss_log,
+            local_step_consumed_tokens=step_consumed_tokens,
+            step_consumed_tokens=reduced_step_consumed_tokens,
+            total_consumed_tokens=self._sft_total_consumed_tokens,
+            data_time=data_time,
+            step_time=step_time,
+            grad_norm=grad_norm,
+            efficient_attn_ratio=other_log["efficient_attn_ratio"],
+        )
+
+        # to return sft log
+        loss_log["grad_norm"] = grad_norm.item()
+        loss_log["data_time"] = data_time
+        loss_log["step_time"] = step_time
+        loss_log["tgs"] = step_consumed_tokens / step_time
+        loss_log["efficient_attn_ratio"] = other_log["efficient_attn_ratio"]
+        return loss_log
+
+    def _next_sft_data_batch(self):
+        try:
+            data = next(self._sft_dataloader_iter)  # type: ignore[assignment]
+        except StopIteration:
+            self._sft_cur_epoch += 1
+            self._sft_dataloader.set_epoch(self._sft_cur_epoch)
+            self._sft_dataloader_iter = iter(self._sft_dataloader)
+            data = next(self._sft_dataloader_iter)
+        return data
+
+    def _train_one_step_sft(self, data_batch):
+        seq_ctx_list: list[SequenceContext] = []
+        loss_ctx_input_list: list[CELossContextInputItem] = []
+        for data in data_batch:
+            seq_ctx = data["seq_ctx"].to(DEVICE)
+            loss_ctx_input = CELossContextInputItem(shifted_labels=data["shifted_labels"]).to(DEVICE)
+            if self.sp_mesh.size() > 1:
+                seq_ctx = seq_ctx.split(sequence_parallel_mesh=self.sp_mesh)
+                loss_ctx_input = loss_ctx_input.sp_split(self.sp_mesh)
+            seq_ctx_list.append(seq_ctx)
+            loss_ctx_input_list.append(loss_ctx_input)
+
+        del data_batch
+
+        LossContext = self._sft_loss_cfg.loss_ctx_cls
+        batches_loss_kwargs = LossContext.build_batches_loss_kwargs(
+            loss_ctx_input_list,
+            self._sft_loss_cfg,
+            cu_seq_lens_list=[seq_ctx.cu_seq_lens_q for seq_ctx in seq_ctx_list],
+            sp_mesh=self.sp_mesh,
+        )
+        engine_input = []
+        for seq_ctx, loss_kwargs in zip(seq_ctx_list, batches_loss_kwargs):
+            loss_ctx = LossContext(
+                loss_cfg=self._sft_loss_cfg,
+                loss_kwargs=loss_kwargs,
+            )
+            engine_input.append(
+                ModelItem(
+                    seq_ctx=seq_ctx,
+                    loss_ctx=loss_ctx,
+                )
+            )
+
+        loss_log, other_log = self._engine.train_step(engine_input)
+        grad_norm = self._engine.clip_grad_norm()
+        self._engine.step_optimizer(grad_norm)
+        return loss_log, other_log, grad_norm
+
+    def _sft_log_step(
+        self,
+        loss_log: LossLog,
+        local_step_consumed_tokens: int,
+        step_consumed_tokens: int,
+        total_consumed_tokens: int,
+        data_time: float,
+        step_time: float,
+        grad_norm: float,
+        efficient_attn_ratio: float,
+    ):
+        tgs = local_step_consumed_tokens / step_time
+        loss_log_list = [f"{k}: {v:.8f}" for k, v in loss_log.items()]
+        loss_log_str = ", ".join(loss_log_list)
+
+        max_memory = DEVICE_MODULE.max_memory_allocated()  # type: ignore[attr-defined]
+        reserved_memory = DEVICE_MODULE.max_memory_reserved()  # type: ignore[attr-defined]
+
+        self.logger.info(
+            f"Rank{self.rank} Step {self._rollout_step}: data_time: {data_time:.4f} time: {step_time:.4f} "
+            f"text_tokens: {local_step_consumed_tokens} "
+            f"step_consumed_tokens: {step_consumed_tokens} "
+            f"total_consumed_tokens: {total_consumed_tokens} "
+            f"efficient_attn_ratio: {efficient_attn_ratio:.4f} "
+            f"{loss_log_str} "
+            f"grad_norm: {grad_norm:.8f} "
+            f"max_memory: {max_memory / (1024**3):.2f} GB "
+            f"reserved_memory: {reserved_memory / (1024**3):.2f} GB "
+            f"tgs: {tgs:.1f} "
+        )
+
+    def _reduce_number_across_rank(self, rank_number: int) -> int:
+        _gathered_list = [None for _ in range(dist.get_world_size())]
+        dist.all_gather_object(_gathered_list, rank_number)
+        reduced_number = sum(_gathered_list)  # type: ignore[arg-type]
+        return reduced_number
 
     @ray_method
     def save_hf(self, hf_dir: str, save_dtype: torch.dtype = torch.bfloat16):
@@ -1181,7 +1373,7 @@ class TrainingWorker(SingleAcceleratorWorker):
         return
 
     @ray_method
-    def save_dcp(self, checkpoint_path: Path | str, no_save_optimizer: bool = False):
+    def save(self, checkpoint_path: Path | str, no_save_optimizer: bool = False):
         """Save the DCP checkpoint of the training worker."""
         if not isinstance(checkpoint_path, Path):
             checkpoint_path = Path(checkpoint_path)
@@ -1193,6 +1385,25 @@ class TrainingWorker(SingleAcceleratorWorker):
             model_dir=model_path,
             optimizer_dir=None if no_save_optimizer else optimizer_path,
         )
+
+        # Save sft dataloader
+        if self.rank == 0 and self._sft_dataloader is not None:
+            sft_dataloader_path = checkpoint_path / self._SAVE_SFT_DATALOADER_DIR
+            dataloader_state = self._sft_dataloader.get_state_dict(self._sft_total_consumed_samples)
+            torch.save(dataloader_state, sft_dataloader_path)
+
+            train_state_path = checkpoint_path / self._SAVE_SFT_TRAIN_STATE_PATH
+            with train_state_path.open("w") as f:
+                f.write(
+                    json.dumps(
+                        {
+                            "cur_step": self._rollout_step,
+                            "cur_epoch": self._sft_cur_epoch,
+                            "total_consumed_samples": self._sft_total_consumed_samples,
+                            "total_consumed_tokens": self._sft_total_consumed_tokens,
+                        }
+                    )
+                )
 
     @ray_method
     def resume(self, load_checkpoint_cfg: LoadCheckpointConfig):
@@ -1220,6 +1431,26 @@ class TrainingWorker(SingleAcceleratorWorker):
             load_states=load_checkpoint_cfg.load_optimizer_states,
             load_args=load_checkpoint_cfg.load_optimizer_args,
         )
+
+        # Resume sft dataloader
+        sft_dataloader_path = resume_from / self._SAVE_SFT_DATALOADER_DIR
+        if self._sft_dataloader is not None:
+            if not sft_dataloader_path.exists():
+                raise FileNotFoundError(f"Dataloader path {sft_dataloader_path} does not exist.")
+            dataloader_state = torch.load(sft_dataloader_path, map_location=DEVICE)
+            self._sft_dataloader.load_state_dict(dataloader_state)
+            self.logger.info(f"Resume sft dataloader from {sft_dataloader_path}")
+
+            train_state_path = resume_from / self._SAVE_SFT_TRAIN_STATE_PATH
+            if not train_state_path.exists():
+                raise FileNotFoundError(f"Train state path {train_state_path} does not exist.")
+            with train_state_path.open("r") as f:
+                train_state = json.loads(f.read())
+                self._rollout_step = train_state["cur_step"]
+                self._sft_cur_epoch = train_state["cur_epoch"]
+                self._sft_total_consumed_samples = train_state["total_consumed_samples"]
+                self._sft_total_consumed_tokens = train_state["total_consumed_tokens"]
+                self.logger.info(f"Resume sft train state from {train_state_path}")
 
     @ray_method
     def ready(self) -> bool:
