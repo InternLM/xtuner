6e24237f7590dc50853c9cef49a6c99248a05cbc
HIT-cwh <2892770585@qq.com>
2026-01-04 20:48:37 +0800

    [Feature] support custom fsdp agrs for pt2.8

--- xtuner/v1/patch/__init__.py
+++ xtuner/v1/patch/__init__.py
@@ -1,5 +1,6 @@
 from . import torch_shape_env_simplify_pt28
 from .torch_dcp_planner import patch_default_save_plan
+from .torch_fsdp_comm import patch_fsdp_agrs
 
 
-__all__ = ["patch_default_save_plan", "torch_shape_env_simplify_pt28"]
+__all__ = ["patch_default_save_plan", "torch_shape_env_simplify_pt28", "patch_fsdp_agrs"]
