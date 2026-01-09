0dacc85d17301f8f8dcacd2b4ed4bb602ed52447
HIT-cwh <2892770585@qq.com>
2026-01-09 18:01:54 +0800

    move to op folder

--- xtuner/v1/ops/comm/__init__.py
+++ xtuner/v1/ops/comm/__init__.py
@@ -1,5 +1,13 @@
 from .all_to_all import all_to_all_single_autograd, ulysses_all_to_all
+from .nvls_agrs import AllGatherManager, ReduceScatterManager, SymmBufferManager
 from .sequence_parallel import split_for_sequence_parallel
 
 
-__all__ = ["all_to_all_single_autograd", "ulysses_all_to_all", "split_for_sequence_parallel"]
+__all__ = [
+    "all_to_all_single_autograd",
+    "ulysses_all_to_all",
+    "split_for_sequence_parallel",
+    "SymmBufferManager",
+    "AllGatherManager",
+    "ReduceScatterManager",
+]
