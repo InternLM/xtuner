2c27906d8998388b1f0026c71a5436f07d780b1a
YanhuiDua <dyh10280@163.com>
2026-02-26 21:00:22 +0800

    Optimizer Sampler && ProduceStrategy: 1. rename SamplerWithReplayBuffer to Sampler and use Sampler defaultly; 2. add is_valid_sample_fn and should_continue_fn arguments to produce_batch

--- tests/ray/test_agent_loop.py
+++ tests/ray/test_agent_loop.py
@@ -12,7 +12,7 @@ from xtuner.v1.rl.base.agent_loop_manager import AgentLoopManager
 from xtuner.v1.data_proto import RolloutState, Status, SampleParams 
 from xtuner.v1.ray.rollout import RolloutController
 from xtuner.v1.ray.judger.gsm8k import GSM8KJudgerConfig
-from xtuner.v1.rl.base.producer import SyncProduceStrategy, Sampler
+from xtuner.v1.rl.base.producer import SyncProduceStrategy, Sampler, SamplerConfig
 from xtuner.v1.rl.base.replay_buffer import ReplayBuffer
 from xtuner.v1.datasets.config import DataloaderConfig, DatasetConfig
 from xtuner.v1.datasets.rl_tokenize_fn import RLTextTokenizeFnConfig
@@ -47,7 +47,7 @@ class TestAgentLoop(unittest.IsolatedAsyncioTestCase):
     def init_config(self):
         self.resources_cfg = AcceleratorResourcesConfig(
             accelerator=resource_map[torch.accelerator.current_accelerator().type],
-            num_workers=8,
+            num_workers=1,
             num_cpus_per_worker=8,
             cpu_memory_per_worker=16 * 1024**3,  # 16 GB
         )
@@ -86,9 +86,11 @@ class TestAgentLoop(unittest.IsolatedAsyncioTestCase):
         sample_params = SampleParams(max_tokens=self.max_response_length, temperature=0.0)
         agent_loop = SingleTurnAgentLoop(rollout_ctl=rollout_controller, sample_params=sample_params, hf_checkpoint=self.model_path, judger=gsm8k_judger)
         # 3. 构造输入数据
+        prompt_repeat_k = 4
         rollout_state = FAKE_INPUT_ITEM
+        group_in_rollout_state = [FAKE_INPUT_ITEM] * prompt_repeat_k
         # 4. 执行 generate_group && generate_sample
-        group_rollout_state = await agent_loop.generate_group(rollout_state, prompt_repeat_k=4)
+        group_rollout_state = await agent_loop.generate_group(group_in_rollout_state)
         single_rollout_state = await agent_loop.generate_sample(rollout_state)
         # 5. 验证结果
         self.assertEqual(len(group_rollout_state), 4)
@@ -110,21 +112,25 @@ class TestAgentLoop(unittest.IsolatedAsyncioTestCase):
         sample_params = SampleParams(max_tokens=self.max_response_length, temperature=0.0)
         agent_loop = SingleTurnAgentLoop(rollout_ctl=rollout_controller, sample_params=sample_params, hf_checkpoint=self.model_path, judger=gsm8k_judger)
         # 3. 创建 AgentLoopManager
-        dataloader_cfg = DataloaderConfig(
-            dataset_config_list=[
-                {
-                    "dataset": DatasetConfig(name="gsm8k",
-                                            anno_path=TRAIN_DATA_PATH,
-                                            sample_ratio=1.0),
-                    "tokenize_fn": RLTextTokenizeFnConfig(max_length=self.max_prompt_length),
-                },
-            ],
-            collator='fake_collator',
-            pack_level='none',
-            group_by_length=False,
+        sampler_config = SamplerConfig(
+            dataloader_cfg=DataloaderConfig(
+                dataset_config_list=[
+                    {
+                        "dataset": DatasetConfig(name="gsm8k",
+                                                anno_path=TRAIN_DATA_PATH,
+                                                sample_ratio=1.0),
+                        "tokenize_fn": RLTextTokenizeFnConfig(max_length=self.max_prompt_length),
+                    },
+                ],
+                collator='fake_collator',
+                pack_level='none',
+                group_by_length=False,
+            ),
+            tokenizer = self.tokenizer,
+            prompt_repeat_k=2,
         )
-        sampler = Sampler(dataloader_cfg=dataloader_cfg, tokenizer=self.tokenizer, prompt_repeat_k=2)
         replay_buffer = ReplayBuffer()
+        sampler = Sampler(sampler_config, replay_buffer)
         stragegy = SyncProduceStrategy()
         agent_loop_manager = AgentLoopManager(agent_loop=agent_loop, produce_strategy=stragegy, sampler=sampler, replay_buffer=replay_buffer, task_name="test_gsm8k")
         # 4. 执行 produce_batch
