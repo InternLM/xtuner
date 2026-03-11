0337c4af74b8f8e816571d9faeef2cc677267e11
Haian Huang(深度眸) <1286304229@qq.com>
2026-03-11 21:56:12 +0800

    Optimizes the sequence parallel (SP) for GatedDeltaNet linear attention (#1558)

--- xtuner/v1/module/attention/gated_deltanet.py
+++ xtuner/v1/module/attention/gated_deltanet.py
@@ -20,15 +20,11 @@ from .attn_outputs import AttnOutputs
 
 
 # Temporary solution: use separate function objects for each call site, Dynamo will cache them separately
-def _all_to_all_conv_pre(x, scatter_dim, gather_dim, mesh):
+def _all_to_all_conv_pre_qk(x, scatter_dim, gather_dim, mesh):
     return ulysses_all_to_all(x, scatter_dim=scatter_dim, gather_dim=gather_dim, mesh=mesh)
 
 
-def _all_to_all_conv_post(x, scatter_dim, gather_dim, mesh):
-    return ulysses_all_to_all(x, scatter_dim=scatter_dim, gather_dim=gather_dim, mesh=mesh)
-
-
-def _all_to_all_qkv(x, scatter_dim, gather_dim, mesh):
+def _all_to_all_conv_pre_v(x, scatter_dim, gather_dim, mesh):
     return ulysses_all_to_all(x, scatter_dim=scatter_dim, gather_dim=gather_dim, mesh=mesh)
 
 
@@ -189,7 +185,7 @@ class GatedDeltaNet(nn.Module):
         self.in_proj_b = build_linear(self.hidden_size, self.num_v_heads, bias=False)
         self.in_proj_a = build_linear(self.hidden_size, self.num_v_heads, bias=False)
 
-    def forward(
+    def forward_for_sp(
         self,
         hidden_states: torch.Tensor,
         seq_ctx: SequenceContext,
@@ -225,24 +221,204 @@ class GatedDeltaNet(nn.Module):
         else:
             seq_idx = seq_ctx.seq_idx
 
-        # TODO: due to the limitation of scatter_dim=1 in ulysses_all_to_all,
-        # the implementation is very inelegant and inefficient, and needs to be refactored in the future.
-        mixed_qkv = mixed_qkv.transpose(1, 2)
+        query, key, value = torch.split(
+            mixed_qkv,  # (1, L/sp_size, 8192)
+            [
+                self.key_dim,
+                self.key_dim,
+                self.value_dim,
+            ],
+            dim=-1,
+        )
+        # (1, L, 8192/sp_size)
+        query = query.transpose(1, 2)  # (1, dim, L/sp_size)
+        key = key.transpose(1, 2)
+        value = value.transpose(1, 2)
+
+        query = _all_to_all_conv_pre_qk(
+            query,
+            scatter_dim=1,
+            gather_dim=2,
+            mesh=seq_ctx.sequence_parallel_mesh,
+        )
+        key = _all_to_all_conv_pre_qk(
+            key,
+            scatter_dim=1,
+            gather_dim=2,
+            mesh=seq_ctx.sequence_parallel_mesh,
+        )
+        value = _all_to_all_conv_pre_v(
+            value,
+            scatter_dim=1,
+            gather_dim=2,
+            mesh=seq_ctx.sequence_parallel_mesh,
+        )
+
+        # query =  (1, dim/sp_size, L)
+        query_weight, key_weight, value_weight = torch.split(
+            weight,  # (8192, 4)
+            [
+                self.key_dim,
+                self.key_dim,
+                self.value_dim,
+            ],
+            dim=0,
+        )
+
+        assert seq_ctx.sequence_parallel_mesh is not None, "sequence_parallel_mesh is required for forward_for_sp"
+        sp_rank = seq_ctx.sequence_parallel_mesh.get_local_rank()
+        sp_size = seq_ctx.sequence_parallel_mesh.size()
+        query_weight = query_weight.chunk(seq_ctx.sequence_parallel_mesh.size(), dim=0)[sp_rank]
+        key_weight = key_weight.chunk(seq_ctx.sequence_parallel_mesh.size(), dim=0)[sp_rank]
+        value_weight = value_weight.chunk(seq_ctx.sequence_parallel_mesh.size(), dim=0)[sp_rank]
+        if bias is not None:
+            bias = bias.chunk(seq_ctx.sequence_parallel_mesh.size(), dim=0)[sp_rank]
+
+        query = query.transpose(1, 2).contiguous().transpose(1, 2)  # make it contiguous for causal_conv1d_fn
+        key = key.transpose(1, 2).contiguous().transpose(1, 2)  # make it contiguous for causal_conv1d_fn
+        value = value.transpose(1, 2).contiguous().transpose(1, 2)  # make it contiguous for causal_conv1d_fn
+        query = self.causal_conv1d_fn(  # query (batch, dim, seqlen)
+            x=query,  # need non contiguous
+            weight=query_weight,
+            bias=bias,
+            activation=self.activation,
+            seq_idx=seq_idx,
+        )
+        key = self.causal_conv1d_fn(
+            x=key,  # need non contiguous
+            weight=key_weight,
+            bias=bias,
+            activation=self.activation,
+            seq_idx=seq_idx,
+        )
+        value = self.causal_conv1d_fn(
+            x=value,  # need non contiguous
+            weight=value_weight,
+            bias=bias,
+            activation=self.activation,
+            seq_idx=seq_idx,
+        )
+
+        beta = b.sigmoid()
+        # If the model is loaded in fp16, without the .float() here, A might be -inf
+        A_log = self.A_log
+        dt_bias = self.dt_bias
+        if isinstance(A_log, DTensor):
+            A_log = A_log.to_local()
+        if isinstance(dt_bias, DTensor):
+            dt_bias = dt_bias.to_local()
+
+        g = -A_log.float().exp() * F.softplus(a.float() + dt_bias)
+
+        # (1,key_dim/sp_size, L)
+        query = query.transpose(1, 2).reshape(
+            batch_size, seq_len * sp_size, -1, self.head_k_dim
+        )  # (1, L, num_k_heads/sp_size, head_k_dim)
+        key = key.transpose(1, 2).reshape(
+            batch_size, seq_len * sp_size, -1, self.head_k_dim
+        )  # (1, L, num_k_heads/sp_size, head_k_dim)
+        value = value.transpose(1, 2).reshape(
+            batch_size, seq_len * sp_size, -1, self.head_v_dim
+        )  # (1, L, num_v_heads/sp_size, head_v_dim)
+
+        if self.num_v_heads // self.num_k_heads > 1:
+            query = query.repeat_interleave(self.num_v_heads // self.num_k_heads, dim=2)
+            key = key.repeat_interleave(self.num_v_heads // self.num_k_heads, dim=2)
+
         if seq_ctx.sequence_parallel_mesh and seq_ctx.sequence_parallel_mesh.size() > 1:
-            sp_rank = seq_ctx.sequence_parallel_mesh.get_local_rank()
-            weight = weight.chunk(seq_ctx.sequence_parallel_mesh.size(), dim=0)[sp_rank]
-            bias = self.conv1d.bias
-            if bias is not None:
-                bias = bias.chunk(seq_ctx.sequence_parallel_mesh.size(), dim=0)[sp_rank]
-            mixed_qkv = _all_to_all_conv_pre(
-                mixed_qkv,  # (1, 8192, L/sp_size)
-                scatter_dim=1,  # must be 1
+            g = g.transpose(1, 2)
+            beta = beta.transpose(1, 2)
+
+            g = _all_to_all_gb(
+                g,  # (1, num_v_heads, L/sp_size)
+                scatter_dim=1,
                 gather_dim=2,
                 mesh=seq_ctx.sequence_parallel_mesh,
             )
-            # contiguous -> non-contiguous
-            mixed_qkv = mixed_qkv.transpose(1, 2).contiguous().transpose(1, 2)
+            beta = _all_to_all_gb(
+                beta,  # (1, num_v_heads, L/sp_size)
+                scatter_dim=1,
+                gather_dim=2,
+                mesh=seq_ctx.sequence_parallel_mesh,
+            )
+            g = g.transpose(1, 2)
+            beta = beta.transpose(1, 2)
 
+        core_attn_out, _ = self.chunk_gated_delta_rule(
+            query,
+            key,
+            value,
+            g=g,
+            beta=beta,
+            initial_state=None,
+            output_final_state=False,
+            use_qk_l2norm_in_kernel=True,
+            cu_seqlens=seq_ctx.cu_seq_lens_q,
+        )
+
+        if seq_ctx.sequence_parallel_mesh and seq_ctx.sequence_parallel_mesh.size() > 1:
+            core_attn_out = _all_to_all_out(
+                core_attn_out,  # (1, L, num_v_head/sp_size, head_dim)
+                scatter_dim=1,
+                gather_dim=2,
+                mesh=seq_ctx.sequence_parallel_mesh,
+            )
+
+        # reshape input data into 2D tensor
+        core_attn_out = core_attn_out.reshape(-1, self.head_v_dim)
+        z = z.reshape(-1, self.head_v_dim)
+
+        core_attn_out = self.norm(core_attn_out, z)
+        core_attn_out = core_attn_out.reshape(batch_size, seq_len, -1)
+
+        output = self.out_proj(core_attn_out)
+        attn_outputs: AttnOutputs = {
+            "projected_output": output,
+        }
+        return attn_outputs
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        seq_ctx: SequenceContext,
+        position_embeddings: tuple[torch.Tensor, torch.Tensor] | None = None,  # not used
+    ) -> AttnOutputs:
+        if seq_ctx.sequence_parallel_mesh and seq_ctx.sequence_parallel_mesh.size() > 1:
+            return self.forward_for_sp(hidden_states, seq_ctx, position_embeddings)
+
+        batch_size, seq_len, _ = hidden_states.shape
+        assert batch_size == 1, "Only batch size of 1 is supported for now in GateDeltaNet"
+        mixed_qkv = self.in_proj_qkv(hidden_states)
+
+        z = self.in_proj_z(hidden_states)
+        z = z.reshape(batch_size, seq_len, -1, self.head_v_dim)
+
+        b = self.in_proj_b(hidden_states)
+        a = self.in_proj_a(hidden_states)
+
+        weight = self.conv1d.weight.squeeze(1)
+        bias = self.conv1d.bias
+        if isinstance(weight, DTensor):
+            weight = weight.to_local()
+        if bias and isinstance(bias, DTensor):
+            bias = bias.to_local()
+
+        # TODO: If full_graph mode is supported in the future, it needs to be modified to custom_op
+        if seq_ctx.seq_idx is None:
+            seq_idx = torch.cat(
+                [
+                    torch.full((s,), i, dtype=torch.int32, device=mixed_qkv.device)
+                    for i, s in enumerate(seq_ctx.seq_lens_q)
+                ],
+                dim=0,
+            )[None]
+            seq_ctx.seq_idx = cast(torch.IntTensor, seq_idx)
+        else:
+            seq_idx = seq_ctx.seq_idx
+
+        # TODO: due to the limitation of scatter_dim=1 in ulysses_all_to_all,
+        # the implementation is very inelegant and inefficient, and needs to be refactored in the future.
+        mixed_qkv = mixed_qkv.transpose(1, 2)
         mixed_qkv = self.causal_conv1d_fn(
             x=mixed_qkv,  # need non contiguous
             weight=weight,
@@ -251,14 +427,6 @@ class GatedDeltaNet(nn.Module):
             seq_idx=seq_idx,
         )
         mixed_qkv = mixed_qkv.transpose(1, 2)
-        if seq_ctx.sequence_parallel_mesh and seq_ctx.sequence_parallel_mesh.size() > 1:
-            mixed_qkv = _all_to_all_conv_post(
-                mixed_qkv,  # (1, L, 8192/sp_size)
-                scatter_dim=1,
-                gather_dim=2,
-                mesh=seq_ctx.sequence_parallel_mesh,
-            )
-
         query, key, value = torch.split(
             mixed_qkv,
             [
@@ -287,49 +455,6 @@ class GatedDeltaNet(nn.Module):
             query = query.repeat_interleave(self.num_v_heads // self.num_k_heads, dim=2)
             key = key.repeat_interleave(self.num_v_heads // self.num_k_heads, dim=2)
 
-        if seq_ctx.sequence_parallel_mesh and seq_ctx.sequence_parallel_mesh.size() > 1:
-            query = query.transpose(1, 2)
-            key = key.transpose(1, 2)
-            value = value.transpose(1, 2)
-            g = g.transpose(1, 2)
-            beta = beta.transpose(1, 2)
-            query = _all_to_all_qkv(
-                query,  # (1, num_q_heads, L/sp_size, head_dim)
-                scatter_dim=1,
-                gather_dim=2,
-                mesh=seq_ctx.sequence_parallel_mesh,
-            )
-            key = _all_to_all_qkv(
-                key,  # (1, num_k_heads, L/sp_size, head_dim)
-                scatter_dim=1,
-                gather_dim=2,
-                mesh=seq_ctx.sequence_parallel_mesh,
-            )
-            value = _all_to_all_qkv(
-                value,  # (1, num_v_heads, L/sp_size, head_dim)
-                scatter_dim=1,
-                gather_dim=2,
-                mesh=seq_ctx.sequence_parallel_mesh,
-            )
-            g = _all_to_all_gb(
-                g,  # (1, num_v_heads, L/sp_size)
-                scatter_dim=1,
-                gather_dim=2,
-                mesh=seq_ctx.sequence_parallel_mesh,
-            )
-            beta = _all_to_all_gb(
-                beta,  # (1, num_v_heads, L/sp_size)
-                scatter_dim=1,
-                gather_dim=2,
-                mesh=seq_ctx.sequence_parallel_mesh,
-            )
-
-            query = query.transpose(1, 2)
-            key = key.transpose(1, 2)
-            value = value.transpose(1, 2)
-            g = g.transpose(1, 2)
-            beta = beta.transpose(1, 2)
-
         core_attn_out, _ = self.chunk_gated_delta_rule(
             query,
             key,
@@ -341,14 +466,6 @@ class GatedDeltaNet(nn.Module):
             use_qk_l2norm_in_kernel=True,
             cu_seqlens=seq_ctx.cu_seq_lens_q,
         )
-        if seq_ctx.sequence_parallel_mesh and seq_ctx.sequence_parallel_mesh.size() > 1:
-            core_attn_out = _all_to_all_out(
-                core_attn_out,  # (1, L, num_v_head/sp_size, head_dim)
-                scatter_dim=1,
-                gather_dim=2,
-                mesh=seq_ctx.sequence_parallel_mesh,
-            )
-
         # reshape input data into 2D tensor
         core_attn_out = core_attn_out.reshape(-1, self.head_v_dim)
         z = z.reshape(-1, self.head_v_dim)
