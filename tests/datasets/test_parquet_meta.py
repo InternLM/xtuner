ad8efe370e055c8e33da3f09f1e7193bae4b6a27
zhaopenghao <henryzhao1989@163.com>
2026-03-13 09:53:37 +0000

    refine unit test

--- tests/datasets/test_parquet_meta.py
+++ tests/datasets/test_parquet_meta.py
@@ -11,7 +11,7 @@ def test_parquet_meta_roundtrip(tmp_path):
         "a": np.array([1, 2, 3], dtype=np.int64),
         "b": np.array([[1.0, 2.0], [3.0, 4.0]], dtype=np.float32),
         # 规则嵌套 list（会在 roundtrip 中变成 ndarray 也可以）
-        "c": [[1, 2], [3, 4]],
+        "c": [[1, 2], [3, 4, 5]],
         "d": [[1.5, 2.5], [3.5, 4.5]],
     }
 
