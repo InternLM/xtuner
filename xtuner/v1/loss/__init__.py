8ec408b184c7eed979661e8642ccfd9a07d1bd42
tina-wen <61722970+tina-wen@users.noreply.github.com>
2026-03-20 18:23:40 +0800

    [Feature] chunk actor logprob computation for memory saving (#1555)
    Co-authored-by: wentiange <tiangewen@qq.com>

--- xtuner/v1/loss/__init__.py
+++ xtuner/v1/loss/__init__.py
@@ -2,6 +2,7 @@ from .base_loss_ctx import BaseLossConfig, BaseLossContext, BaseLossKwargs
 from .ce_loss import CELossConfig, CELossContext
 from .chunk_loss import ChunkLoss
 from .moe_loss import BalancingLoss, ZLoss
+from .rl_loss import LogProbConfig, LogProbContext
 
 
 __all__ = [
@@ -13,6 +14,8 @@ __all__ = [
     "BaseLossConfig",
     "BaseLossContext",
     "BaseLossKwargs",
+    "LogProbConfig",
+    "LogProbContext",
 ]
 
 import torch
