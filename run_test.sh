40a734f1482a6fb39ac08ef0472bd5ef767ee8f2
zhaopenghao <henryzhao1989@163.com>
2026-03-23 12:17:30 +0000

    remove helper files

--- run_test.sh
+++
@@ -1,7 +0,0 @@
-
-cd $(dirname $0)
-source ./zdev/env.sh
-source $(conda info --base)/etc/profile.d/conda.sh
-conda activate py312-pt28-raw
-
-pytest -v -s tests/datasets/test_preset_pack_dataset.py
