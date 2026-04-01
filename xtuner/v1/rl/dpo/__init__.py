1a090e55c257f17c30aa27189233346e44a27e34
LarryLeeee <szli24@m.fudan.edu.cn>
2026-04-01 14:38:27 +0800

    add_mpo

---
+++ xtuner/v1/rl/dpo/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .loss import DPOLossConfig, DPOLossContext, DPOLossContextInputItem
+
+
+__all__ = [
+    "DPOLossConfig",
+    "DPOLossContext",
+    "DPOLossContextInputItem",
+]
