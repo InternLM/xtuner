4396d2e763f1d42dbd2efe680a346ef9290c902f
zhaopenghao <henryzhao1989@163.com>
2026-04-16 02:51:34 +0000

    update planning artifacts

--- run_test.sh
+++ run_test.sh
@@ -3,5 +3,5 @@ source ./zdev/env.sh
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate py312-pt28
 
-pytest -qq tests/rl/test_agent_loop.py \
+pytest -q tests/rl/test_agent_loop.py \
   tests/rl/test_multi_task_agent_loop_manager.py
