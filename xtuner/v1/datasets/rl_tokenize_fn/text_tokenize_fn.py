169c1a5db0cc1f0fa8db6362ad6ebf15859f338b
YanhuiDua <dyh10280@163.com>
2026-04-23 16:43:45 +0800

    fix tokenize_fn

--- xtuner/v1/datasets/rl_tokenize_fn/text_tokenize_fn.py
+++ xtuner/v1/datasets/rl_tokenize_fn/text_tokenize_fn.py
@@ -5,25 +5,28 @@ from transformers import PreTrainedTokenizer
 from xtuner.v1.data_proto.rl_data import RolloutState
 from xtuner.v1.utils import get_logger
 
-from ..data_item import CacheItem
 from ..utils import CachableTokenizeFunction
 
 
 logger = get_logger()
 
 
-class RLTextTokenizeFn(CachableTokenizeFunction[RolloutState | CacheItem]):
+class RLTextTokenizeFn(CachableTokenizeFunction[RolloutState]):
     def __init__(
         self,
         tokenizer: PreTrainedTokenizer,
         max_length: int | None = None,
         tools_schema: list | None = None,
+        data_judger_mapping: dict | None = None,
+        system_prompt: str | None = None,
     ):
         super().__init__(tokenizer)
         self.max_length = max_length
         self.tools_schema = tools_schema if tools_schema is not None else []
+        self.data_judger_mapping = data_judger_mapping
+        self.system_prompt = system_prompt
 
-    def __call__(self, item: dict, **kwargs) -> RolloutState | CacheItem:
+    def __call__(self, item: dict, **kwargs) -> RolloutState:
         """example:
         item = {
                 "data_source": data_source,
@@ -47,6 +50,10 @@ class RLTextTokenizeFn(CachableTokenizeFunction[RolloutState | CacheItem]):
         extra_info = item.get("extra_info", {})
         message = item["prompt"]
 
+        if self.system_prompt:
+            if message[0]["role"] == "system":
+                message = message[1:]
+            message = [{"role": "system", "content": self.system_prompt}] + message
         raw_prompt = self.tokenizer.apply_chat_template(
             message, tools=self.tools_schema, add_generation_prompt=True, tokenize=False
         )
@@ -58,21 +65,27 @@ class RLTextTokenizeFn(CachableTokenizeFunction[RolloutState | CacheItem]):
         if self.state == "cache":
             if self.max_length is not None and num_tokens > self.max_length:
                 num_tokens = 0  # will be filtered out by the dataset filter
-            return {
-                "num_tokens": num_tokens,
-                "proxy_attn_flops": float(num_tokens),
-            }
         else:
             if self.max_length is not None:
                 assert num_tokens <= self.max_length, f"num_tokens {num_tokens} > max_length {self.max_length}"
 
+        mapped_judger_name_and_weight = None
+        if self.state != "cache":
+            data_source = item.get("data_source")
+            assert data_source is not None, "data_source is required in item"
+            extra_info["origin_data_source"] = data_source
+            if self.data_judger_mapping is not None:
+                mapped_judger_name_and_weight = self.data_judger_mapping.get(data_source)
+            else:
+                mapped_judger_name_and_weight = {data_source: 1.0}
+
         rollout_state = RolloutState(
             prompt_ids=prompt_token_ids,
             message=message,
-            data_source=item.get("data_source", "default"),
             reward_model=item.get("reward_model", {}),
             num_tokens=num_tokens,
             proxy_attn_flops=float(num_tokens),
+            data_source=mapped_judger_name_and_weight,
             extra_fields=extra_info,
         )
         return rollout_state
