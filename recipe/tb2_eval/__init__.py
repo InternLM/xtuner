aed2a1e1f3634e50fe188fdbac1c652fa2bd0c92
liukuikun <641417025@qq.com>
2026-05-21 12:06:10 +0000

    update

--- recipe/tb2_eval/__init__.py
+++ recipe/tb2_eval/__init__.py
@@ -1,5 +1,6 @@
 """tb2-eval bench adapter for the rl_task framework."""
 
+from .local_run.dataset import TB2EvalBench
 from .pipeline import runner
 
-__all__ = ["runner"]
+__all__ = ["TB2EvalBench", "runner"]
