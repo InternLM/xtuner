8bb8a7fd76c9558380fa0bb5a2f39145060e35e9
liukuikun <24622904+Harold-lkk@users.noreply.github.com>
2026-05-27 11:24:37 +0800

    remove recipe (#1846)

--- recipe/tb2_eval/__init__.py
+++
@@ -1,6 +0,0 @@
-"""tb2-eval bench adapter for the sandbox_agent_loop framework."""
-
-from .local_run.dataset import TB2EvalBench
-from .pipeline import runner
-
-__all__ = ["TB2EvalBench", "runner"]
