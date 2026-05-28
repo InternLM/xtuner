7a2db9972d6a0516fabf0372c0da327285a13451
zhaopenghao <henryzhao1989@163.com>
2026-05-28 06:42:28 +0000

    DeterministicDDPTestCase use new set_deterministic func

--- xtuner/_testing/utils.py
+++ xtuner/_testing/utils.py
@@ -2,6 +2,7 @@ import os
 import torch
 from contextlib import contextmanager
 import io
+from xtuner.v1.utils.misc import set_deterministic
 
 
 
@@ -10,12 +11,13 @@ def enable_full_determinism():
     Helper function for reproducible behavior during distributed training. See
     - https://pytorch.org/docs/stable/notes/randomness.html for pytorch
     """
+    set_deterministic(True)
+
     #  Enable PyTorch deterministic mode. This potentially requires either the environment
     #  variable 'CUDA_LAUNCH_BLOCKING' or 'CUBLAS_WORKSPACE_CONFIG' to be set,
     # depending on the CUDA version, so we set them both here
     os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":16:8"
-
     # torch.use_deterministic_algorithms(True, warn_only=True)
     torch.set_deterministic_debug_mode(0)
 
