fbbf31f64e79756dac7d585488c2a1c56aa44ef4
Pengcheng Shi <spc117369@gmail.com>
2026-08-07 03:12:14 +0000

    Fix bug: weight update flow when skip_load_weights

--- xtuner/v1/rl/weight_update/transport.py
+++ xtuner/v1/rl/weight_update/transport.py
@@ -73,6 +73,10 @@ class WeightTransport(ABC, Generic[AdapterT]):
 
         self.rollout_url = self.rollout_info.rollout_url
 
+    def reset_rollout_info(self, rollout_info: RolloutWeightUpdateInfo):
+        self.rollout_info = rollout_info
+        self.rollout_url = rollout_info.rollout_url
+
     @staticmethod
     def post_json(url: str, endpoint: str, payload: dict, *, api_key=None) -> dict:
         headers = {"Content-Type": "application/json"}
